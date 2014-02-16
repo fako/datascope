bb89981cdbecddbac286f638d0d7e7abd1b64233
Fako <email@fakoberkers.nl>
2014-02-16 12:13:07 +0100

    Adding global links.

--- urls.py
+++ urls.py
@@ -7,4 +7,7 @@ urlpatterns = patterns('HIF.views',
     url(r'^image-translations/plain/$', ImageTranslationsService.HIF_plain.as_view(), {"Service": ImageTranslationsService}, name='image-translations-plain'),
     url(r'^video-translations/$', VideoTranslationsService.HIF_main.as_view(), {"Service": VideoTranslationsService}, name='video-translations'),
     url(r'^video-translations/plain/$', VideoTranslationsService.HIF_plain.as_view(), {"Service": VideoTranslationsService}, name='video-translations-plain'),
+
+    url(r'^global-image-translations/plain/$', ImageTranslationsService.HIF_plain.as_view(), {"Service": ImageTranslationsService}, name='global-image-translations-plain'),
+    url(r'^global-video-translations/plain/$', VideoTranslationsService.HIF_plain.as_view(), {"Service": VideoTranslationsService}, name='global-video-translations-plain'),
 )
