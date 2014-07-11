c3ef84322a0855fa8182d127c09d57044e060871
fako <email@fakoberkers.nl>
2014-07-11 21:35:35 +0200

    More custom headers to make cross-origin work.

--- middleware.py
+++ middleware.py
@@ -12,5 +12,7 @@ class AllowOriginMiddleware(object):
         origin = request.META.get("HTTP_ORIGIN")
         if origin in domain_settings.HIF_allowed_origins:
             response["Access-Control-Allow-Origin"] = origin
+            response['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+            response['Access-Control-Allow-Headers'] = 'origin, x-csrftoken, content-type, accept'
 
         return response
