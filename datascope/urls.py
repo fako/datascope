4186aa82b249d9c1c2beba96d1114c9a043276a4
fako <email@fakoberkers.nl>
2014-10-28 10:39:17 +0100

    Add core routes to main router.

--- datascope/urls.py
+++ datascope/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'datascope.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'', include('core.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
