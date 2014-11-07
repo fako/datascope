5820f0b7589c5dc641efafead63c2605e7f6c314
fako <email@fakoberkers.nl>
2014-11-07 17:58:26 +0100

    Command to delete storage from db.

---
+++ core/management/commands/clear_storage.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+
+from core.helpers.storage import get_hif_model
+
+
+class Command(BaseCommand):
+    """
+    Clears TextStorage and/or ProcessStorage from the database.
+    """
+
+    def handle(self, *args, **kwargs):
+
+        if not args:
+            args = ['TextStorage', 'ProcessStorage']
+
+        for arg in args:
+            model = get_hif_model(arg)
+            model.objects.all().delete()
