3c3e507bcae7992c712c6901f3d357392e71b99c
fako <email@fakoberkers.nl>
2014-11-10 16:17:26 +0100

    Add search to admin.

--- core/admin.py
+++ core/admin.py
@@ -5,9 +5,11 @@ from core.models import TextStorage, ProcessStorage
 
 class TextStorageAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'status', 'head', 'body', 'created_at']
+    search_fields = ['head', 'body']
 
 class ProcessStorageAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'status', 'results', 'exception', 'traceback', 'created_at', 'modified_at']
+    search_fields = ['results']
 
 admin.site.register(TextStorage, TextStorageAdmin)
 admin.site.register(ProcessStorage, ProcessStorageAdmin)
