fdceb226818830277945b2cdecd72eee3527444e
fako <email@fakoberkers.nl>
2014-11-10 16:25:36 +0100

    Better search fields.

--- core/admin.py
+++ core/admin.py
@@ -5,11 +5,11 @@ from core.models import TextStorage, ProcessStorage
 
 class TextStorageAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'status', 'head', 'body', 'created_at']
-    search_fields = ['head', 'body']
+    search_fields = ['identity', 'arguments', 'configuration']
 
 class ProcessStorageAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'status', 'results', 'exception', 'traceback', 'created_at', 'modified_at']
-    search_fields = ['results']
+    search_fields = ['identity', 'arguments', 'configuration']
 
 admin.site.register(TextStorage, TextStorageAdmin)
 admin.site.register(ProcessStorage, ProcessStorageAdmin)
