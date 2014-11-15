353745ef457841595abd2c9a74d1c835fe4dc17e
fako <email@fakoberkers.nl>
2014-11-15 16:13:54 +0000

    Adding stricter filtering to stats (pop people only).

--- core/management/commands/city_celebrities_stats.py
+++ core/management/commands/city_celebrities_stats.py
@@ -32,13 +32,62 @@ class Command(BaseCommand):
 
         for arg in args:
 
+            interesting_claims = [
+                {
+                    "property": "P106",
+                    "item": 33999  # actor
+                },
+                {
+                    "property": "P106",
+                    "item": 177220  # singer
+                },
+                {
+                    "property": "P106",
+                    "item": 488205  # singer-song writer
+                },
+                {
+                    "property": "P106",
+                    "item": 483501  # artist
+                },
+                {
+                    "property": "P106",
+                    "item": 2066131  # sports person
+                },
+                {
+                    "property": "P106",
+                    "item": 11513337  # athlete competitor
+                },
+                {
+                    "property": "P106",
+                    "item": 10800557  # film actor
+                },
+
+            ]
+
+
+
             # Start the process
             city, coords = arg.split('/')
             print("Fetching information for: {} at {}".format(city, coords))
             city_celebrities = CityCelebritiesService()
             city_celebrities.setup(query=unicode(city))
 
+            interesting_locations = []
             for location in city_celebrities.content:
+
+                interesting_persons = []
+                for person in location['people']:
+
+                    for interesting_claim in interesting_claims:
+                        if interesting_claim in person['claims']:
+                            interesting_persons.append(person)
+                            break
+
+                if len(interesting_persons):
+                    location['people'] = interesting_persons
+                    interesting_locations.append(location)
+
+            for location in interesting_locations:
                 print location['title'], ' > ', len(location['people'])
                 print [person['title'] for person in location['people']]
-                print ''
+                print
