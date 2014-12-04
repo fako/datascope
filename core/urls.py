bebde335b3421504e5a2779322ab04a39a7da87e
fako <email@fakoberkers.nl>
2014-12-04 14:24:37 +0100

    Append slash fix for Angular 1.23

--- core/urls.py
+++ core/urls.py
@@ -4,13 +4,13 @@ from core.output.http.services.manifests import VisualTranslationsService, Peopl
 
 
 urlpatterns = patterns('core.views',
-    url(r'^visual-translations/$', VisualTranslationsService.HIF_main.as_view(), {"Service": VisualTranslationsService}, name='visual-translations'),
-    url(r'^visual-translations/plain/$', VisualTranslationsService.HIF_plain.as_view(), {"Service": VisualTranslationsService}, name='visual-translations-plain'),
+    url(r'^visual-translations/?$', VisualTranslationsService.HIF_main.as_view(), {"Service": VisualTranslationsService}, name='visual-translations'),
+    url(r'^visual-translations/plain/?$', VisualTranslationsService.HIF_plain.as_view(), {"Service": VisualTranslationsService}, name='visual-translations-plain'),
 
-    url(r'^people-suggestions/$', PeopleSuggestionsService.HIF_main.as_view(), {"Service": PeopleSuggestionsService}, name='people-suggestions'),
-    url(r'^people-suggestions/plain/$', PeopleSuggestionsService.HIF_plain.as_view(), {"Service": PeopleSuggestionsService}, name='people-suggestions-plain'),
+    url(r'^people-suggestions/?$', PeopleSuggestionsService.HIF_main.as_view(), {"Service": PeopleSuggestionsService}, name='people-suggestions'),
+    url(r'^people-suggestions/plain/?$', PeopleSuggestionsService.HIF_plain.as_view(), {"Service": PeopleSuggestionsService}, name='people-suggestions-plain'),
 
-    url(r'^city-celebrities/$', CityCelebritiesService.HIF_main.as_view(), {"Service": CityCelebritiesService}, name='city-celebrities'),
+    url(r'^city-celebrities/?$', CityCelebritiesService.HIF_main.as_view(), {"Service": CityCelebritiesService}, name='city-celebrities'),
 
     url(r'question/?', 'question', name='question')
 )
--- datascope/settings.py
+++ datascope/settings.py
@@ -21,6 +21,8 @@ ALLOWED_HOSTS = [
     '.data-scope.org',
 ]
 
+APPEND_SLASH = False
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
