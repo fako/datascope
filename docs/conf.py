c90956bdc0f5faa3010e81f12915f655629153b6
fako <email@fakoberkers.nl>
2015-02-23 21:06:21 +0100

    Use read_the_docs theme for sphinx.

--- docs/conf.py
+++ docs/conf.py
@@ -15,10 +15,15 @@
 import sys
 import os
 
+import sphinx_rtd_theme
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+
+# setup django settings
+os.environ['DJANGO_SETTINGS_MODULE'] = 'datascope.settings'
 
 # -- General configuration ------------------------------------------------
 
@@ -104,15 +109,15 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = "sphinx_rtd_theme"
 
-# Theme options are theme-specific and customize the look and feel of a theme
+# Add any paths that contain custom themes here, relative to this directory.
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
