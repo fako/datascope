446702906e91a9fb2b3034bca00f9d3cd35059ad
fako <email@fakoberkers.nl>
2015-05-17 17:47:14 +0200

    Further development of celery processor.

--- core/processors/resources.py
+++ core/processors/resources.py
@@ -1,6 +1,7 @@
 from django.apps import apps as django_apps
 
-from celery.contrib.methods import task
+from celery import current_app as app
+from celery.contrib.methods import task_method
 
 from core.utils.configuration import ConfigurationProperty
 from core.configuration import DEFAULT_CONFIGURATION
@@ -18,7 +19,7 @@ class HttpResourceProcessor(object):
     config = ConfigurationProperty(
         storage_attribute="_config",
         defaults=DEFAULT_CONFIGURATION,
-        private=['_resource'],
+        private=["_resource", "_configuration_limit", "_batch_size"],
         namespace=""
     )
 
@@ -26,20 +27,42 @@ class HttpResourceProcessor(object):
         super(HttpResourceProcessor, self).__init__()
         assert isinstance(configuration, dict) and "_resource" in configuration, \
             "HttpFetch expects a resource that it should fetch in the configuration."
+
         if "continuation_limit" not in configuration:
-            configuration["continuation_limit"] = 1
+            configuration["_continuation_limit"] = 1
+        if "batch_size" not in configuration:
+            configuration["_batch_size"] = 0
+
         self.config = configuration
 
     def get_link(self):
         Resource = django_apps.get_model("core", self.config.resource)
         return Resource(config=self.config.to_dict(protected=True))
 
-    @task(name="HttpFetch.fetch")
+    @app.task(name="HttpFetch.fetch", filter=task_method)
     def fetch(self, *args, **kwargs):
-        link = self.get_link()
-        link.get(*args, **kwargs)
-        link.save()
-        return [link.id]
+        print self
+        print args
+        print kwargs
+        # Set vars
+        results = []
+        has_next_request = True
+        current_request = {}
+        count = 0
+        limit = self.config.continuation_limit
+        # Continue as long as there are subsequent requests
+        while has_next_request and count < limit:
+            # Get payload
+            link = self.get_link()
+            link.request = current_request
+            link.get(*args, **kwargs)
+            link.save()
+            results.append(link.id)
+            # Prepare next request
+            has_next_request = current_request = link.create_next_request()
+            count += 1
+        # Output results
+        return results
 
     def fetch_mass(self, *args, **kwargs):
         pass
