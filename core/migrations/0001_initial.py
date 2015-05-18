da8f2c3d869ab0f624606361167edb89f46eb4ea
fako <email@fakoberkers.nl>
2015-05-18 10:08:28 +0200

    Update migration with stricter creation rules.

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -5,6 +5,7 @@ from django.db import models, migrations
 import jsonfield.fields
 import core.utils.configuration
 import django.contrib.auth.models
+import core.models.organisms.protocols
 from django.conf import settings
 
 
@@ -34,19 +35,20 @@ class Migration(migrations.Migration):
             name='HttpResourceMock',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('uri', models.CharField(max_length=255, db_index=True)),
+                ('uri', models.CharField(default=None, max_length=255, db_index=True)),
                 ('post_data', models.CharField(default=b'', max_length=255, db_index=True)),
-                ('request', jsonfield.fields.JSONField(default={})),
-                ('head', jsonfield.fields.JSONField(default={})),
-                ('body', models.TextField()),
-                ('status', models.PositiveIntegerField(default=0)),
+                ('request', jsonfield.fields.JSONField(default=None)),
+                ('head', jsonfield.fields.JSONField(default=None)),
+                ('body', models.TextField(default=None)),
+                ('status', models.PositiveIntegerField(default=None)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('modified_at', models.DateTimeField(auto_now=True)),
                 ('purge_at', models.DateTimeField(null=True, blank=True)),
-                ('config', core.utils.configuration.ConfigurationField(default={})),
+                ('config', core.utils.configuration.ConfigurationField()),
             ],
             options={
                 'abstract': False,
             },
+            bases=(models.Model, core.models.organisms.protocols.OrganismInputProtocol),
         ),
     ]
