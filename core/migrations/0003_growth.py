1397215340e04b3c8e76ec327fc14e5d8fa57f21
fako <email@fakoberkers.nl>
2015-06-04 10:38:01 +0200

    Flatten growth migrations.

--- core/migrations/0003_growth.py
+++ core/migrations/0003_growth.py
@@ -8,6 +8,7 @@ import core.utils.configuration
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
         ('core', '0002_collective_individual'),
     ]
 
@@ -17,14 +18,17 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('type', models.CharField(max_length=255)),
-                ('input', models.CharField(max_length=255)),
                 ('config', core.utils.configuration.ConfigurationField(default={})),
                 ('process', models.CharField(max_length=255, choices=[('HttpResourceProcessor.fetch', 'Fetch content from HTTP resource'), ('HttpResourceProcessor.fetch_mass', 'Fetch content from multiple HTTP resources')])),
-                ('success', models.CharField(max_length=255, choices=[('HttpResourceProcessor.fetch', 'Fetch content from HTTP resource'), ('HttpResourceProcessor.fetch_mass', 'Fetch content from multiple HTTP resources')])),
-                ('output', models.CharField(max_length=255)),
-                ('task_id', models.CharField(max_length=255, null=True, blank=True)),
-                ('state', models.CharField(default='New', max_length=255, choices=[(b'RETRY', 'Retry'), (b'ERROR', 'Error'), (b'NEW', 'New'), (b'PROCESSING', 'Processing'), (b'FINISHED', 'Finished')])),
-                ('is_finished', models.BooleanField(default=False)),
+                ('contribute', models.CharField(default=None, max_length=255, null=True, choices=[('HttpResourceProcessor.fetch', 'Fetch content from HTTP resource'), ('HttpResourceProcessor.fetch_mass', 'Fetch content from multiple HTTP resources')])),
+                ('contribute_type', models.CharField(max_length=255, choices=[('append', 'Add contributions to the output.')])),
+                ('input_id', models.PositiveIntegerField()),
+                ('output_id', models.PositiveIntegerField()),
+                ('result_id', models.CharField(max_length=255, null=True, blank=True)),
+                ('state', models.CharField(default='New', max_length=255, db_index=True, choices=[(b'RETRY', 'Retry'), (b'ERROR', 'Error'), (b'NEW', 'New'), (b'PROCESSING', 'Processing'), (b'FINISHED', 'Finished')])),
+                ('is_finished', models.BooleanField(default=False, db_index=True)),
+                ('input_type', models.ForeignKey(related_name='+', to='contenttypes.ContentType')),
+                ('output_type', models.ForeignKey(related_name='+', to='contenttypes.ContentType')),
             ],
         ),
     ]
