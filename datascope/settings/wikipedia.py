12a3a1a83af36473e8edb8074dc41ef1ad26e4c5
fako <email@fakoberkers.nl>
2015-06-13 14:19:13 +0200

    Add labs as db host to wikipedia settings.

--- datascope/settings/wikipedia.py
+++ datascope/settings/wikipedia.py
@@ -4,3 +4,4 @@ DATABASES["default"]["ENGINE"] = 'django.db.backends.mysql'
 DATABASES["default"]["NAME"] = 's52573__datascope'
 DATABASES["default"]["USER"] = 's52573'
 DATABASES["default"]["PASSWORD"] = WIKILABS_MYSQL_PASSWORD
+DATABASES["default"]["HOST"] = 'tools-db'
