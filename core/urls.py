0dba8d429b5b9db268ca5e99c83924ff7a5ead21
fako <email@fakoberkers.nl>
2015-06-13 14:15:42 +0200

    Move urlpatterns to algorithm apps instead of core.

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 
 from django.conf.urls import url
 
-from wiki_news.models import WikiNewsCommunity
+from wiki_news.urls import urlpatterns as wiki_news_patterns
 from . import views
 
 urlpatterns = [
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^collective/(?P<pk>\d+)/$', views.CollectiveView.as_view()),
     url(r'^individual/(?P<pk>\d+)/content/$', views.IndividualContentView.as_view()),
     url(r'^individual/(?P<pk>\d+)/$', views.IndividualView.as_view()),
-    url(r'^wiki-algo-news/$', views.CommunityView.as_view(), {"community_class": WikiNewsCommunity})
 ]
+
+urlpatterns += wiki_news_patterns
