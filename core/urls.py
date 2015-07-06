6ff4076121ab97a97c759b9de8e31093600e7c7c
fako <email@fakoberkers.nl>
2015-07-06 11:23:46 +0200

    Update URL's to work with Collective.url

--- core/urls.py
+++ core/urls.py
@@ -6,10 +6,10 @@ from wiki_news.urls import urlpatterns as wiki_news_patterns
 from . import views
 
 urlpatterns = [
-    url(r'^collective/(?P<pk>\d+)/content/$', views.CollectiveContentView.as_view()),
-    url(r'^collective/(?P<pk>\d+)/$', views.CollectiveView.as_view()),
-    url(r'^individual/(?P<pk>\d+)/content/$', views.IndividualContentView.as_view()),
-    url(r'^individual/(?P<pk>\d+)/$', views.IndividualView.as_view()),
+    url(r'^collective/(?P<pk>\d+)/content/$', views.CollectiveContentView.as_view(), name="collective-content"),
+    url(r'^collective/(?P<pk>\d+)/$', views.CollectiveView.as_view(), name="collective"),
+    url(r'^individual/(?P<pk>\d+)/content/$', views.IndividualContentView.as_view(), name="individual"),
+    url(r'^individual/(?P<pk>\d+)/$', views.IndividualView.as_view(), name="individual-content"),
 ]
 
 urlpatterns += wiki_news_patterns
