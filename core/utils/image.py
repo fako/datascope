f1c5d540db6892495935d48488ae9a3849ff0ee6
fako <email@fakoberkers.nl>
2015-08-16 00:15:20 +0200

    Finish first version of multi-cell span.

--- core/utils/image.py
+++ core/utils/image.py
@@ -7,6 +7,10 @@ class NoCellAvailable(Exception):
     pass
 
 
+class ImageRejected(Exception):
+    pass
+
+
 class ImageGrid(object):
 
     def __init__(self, columns, rows, cell_width, cell_height, scale_margin):
@@ -24,6 +28,27 @@ class ImageGrid(object):
     def fill_from_src(self, sources):
         pass
 
+    def center_image(self, image, delta_width, delta_height, horizontal, vertical):
+        assert isinstance(image, Image.Image), "Center image can only be done with a PIL image"
+        assert delta_width >= 0, "Can't center an image if its delta_width is {}".format(delta_width)
+        assert delta_height >= 0, "Can't center an image if its delta_height is {}".format(delta_height)
+
+        if delta_width % 2:
+            delta_width -= 1
+        if delta_height % 2:
+            delta_height -= 1
+        offset_width = int(delta_width / 2)
+        offset_height = int(delta_height / 2)
+
+        box = (
+            offset_width,
+            offset_height,
+            offset_width + self.cell_width * horizontal,
+            offset_height + self.cell_height * vertical,
+        )
+        image.crop(box)
+        return image
+
     def validate_image_size(self, image, horizontal=1, vertical=1):
         if horizontal > self.columns:
             raise NoCellAvailable("Image spans more columns than available")
@@ -31,18 +56,28 @@ class ImageGrid(object):
             raise NoCellAvailable("Image spans more rows than available")
 
         image_width, image_height = image.size
-        delta_width = image_width - self.cell_width * horizontal + self.scale_margin
-        delta_height = image_height - self.cell_height * vertical + self.scale_margin
+        delta_width = image_width - self.cell_width * horizontal
+        delta_height = image_height - self.cell_height * vertical
         is_landscape = image_width >= image_height
 
+        if delta_width >= self.cell_width - self.scale_margin and is_landscape:
+            return self.validate_image_size(image, horizontal+1, 1)
+        elif delta_height >= self.cell_height - self.scale_margin and not is_landscape:
+            return self.validate_image_size(image, 1, vertical+1)
+
         if delta_width >= 0 and delta_height >= 0:
-            if delta_width % 2:
-                delta_width -= 1
-            if delta_height % 2:
-                delta_height -= 1
-            offset_width = int(delta_width / 2)
-            offset_height = int(delta_height / 2)
-            return offset_width, offset_height, offset_width + self.cell_width, offset_height + self.cell_height
+            return self.center_image(image, delta_width, delta_height, horizontal, vertical)
+
+        if -1 * delta_width > self.scale_margin or -1 * delta_height > self.scale_margin:
+            raise ImageRejected("Image with delta {}/{} is too small for set scale margin {}".format(
+                delta_width,
+                delta_height,
+                self.scale_margin
+            ))
+
+        print("Delta: ", delta_width, delta_height)
+        # if delta_width < 0 or delta_height < 0 and is_landscape:
+        #     image.resize
 
 
     def cell_image(self, image):
