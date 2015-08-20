e58d87fda9bd9b68689bbff94a087862e5c7a9d1
fako <email@fakoberkers.nl>
2015-08-20 14:11:22 +0200

    Take care of too small images and prepare grid validation.

--- core/utils/image.py
+++ core/utils/image.py
@@ -47,27 +47,32 @@ class ImageGrid(object):
             offset_height + self.cell_height * vertical,
         )
         image.crop(box)
-        return image
+        return image, horizontal, vertical
 
     def validate_image_size(self, image, horizontal=1, vertical=1):
+        # Validate grid (TODO: move)
         if horizontal > self.columns:
             raise NoCellAvailable("Image spans more columns than available")
         if vertical > self.rows:
             raise NoCellAvailable("Image spans more rows than available")
 
+        # Calculate image properties
         image_width, image_height = image.size
         delta_width = image_width - self.cell_width * horizontal
         delta_height = image_height - self.cell_height * vertical
         is_landscape = image_width >= image_height
 
-        if delta_width >= self.cell_width - self.scale_margin and is_landscape:
+        # See if image spans multiple cells or not
+        if delta_width >= self.cell_width * horizontal - self.scale_margin and is_landscape:
             return self.validate_image_size(image, horizontal+1, 1)
-        elif delta_height >= self.cell_height - self.scale_margin and not is_landscape:
+        elif delta_height >= self.cell_height * vertical - self.scale_margin and not is_landscape:
             return self.validate_image_size(image, 1, vertical+1)
 
+        # Center image if it overflows the cell(s)
         if delta_width >= 0 and delta_height >= 0:
             return self.center_image(image, delta_width, delta_height, horizontal, vertical)
 
+        # Reject image if it is smaller than the cell and can't be scaled up within allowed limits
         if -1 * delta_width > self.scale_margin or -1 * delta_height > self.scale_margin:
             raise ImageRejected("Image with delta {}/{} is too small for set scale margin {}".format(
                 delta_width,
@@ -76,6 +81,7 @@ class ImageGrid(object):
             ))
 
         print("Delta: ", delta_width, delta_height)
+        return image, horizontal, vertical
         # if delta_width < 0 or delta_height < 0 and is_landscape:
         #     image.resize
 
--- core/utils/tests/image.py
+++ core/utils/tests/image.py
@@ -5,7 +5,7 @@ from PIL import Image
 from unittest import TestCase
 from mock import Mock
 
-from core.utils.image import ImageGrid
+from core.utils.image import ImageGrid, ImageRejected
 
 
 
@@ -13,6 +13,7 @@ class TestImageGrid(TestCase):
 
     def setUp(self):
         self.image_grid = ImageGrid(4, 3, 16, 9, 3)
+
         self.fit = Mock(Image.Image, size=(16, 9))
         self.oversize_x = Mock(Image.Image, size=(18, 9))
         self.oversize_y = Mock(Image.Image, size=(16, 11))
@@ -20,6 +21,9 @@ class TestImageGrid(TestCase):
         self.oversize_xyr = Mock(Image.Image, size=(17, 10))
         self.kingsize_x = Mock(Image.Image, size=(32, 12))
         self.kingsize_y = Mock(Image.Image, size=(16, 20))
+        self.too_small_x = Mock(Image.Image, size=(12, 9))
+        self.too_small_y = Mock(Image.Image, size=(16, 5))
+        self.too_small_xy = Mock(Image.Image, size=(5, 5))
 
     def test_init(self):
         ig = ImageGrid(4, 3, 16, 9, 0)
@@ -28,17 +32,48 @@ class TestImageGrid(TestCase):
         self.assertEqual(len(ig.cells), 25)
 
     def test_validate_image_size(self):
-        image = self.image_grid.validate_image_size(self.fit)
+        image, horizontal, vertical = self.image_grid.validate_image_size(self.fit)
         image.crop.assert_called_once_with((0, 0, 16, 9,))
-        image = self.image_grid.validate_image_size(self.oversize_x)
+        self.assertEqual(horizontal, 1)
+        self.assertEqual(vertical, 1)
+        image, horizontal, vertical = self.image_grid.validate_image_size(self.oversize_x)
         image.crop.assert_called_once_with((1, 0, 17, 9,))
-        image = self.image_grid.validate_image_size(self.oversize_y)
+        self.assertEqual(horizontal, 1)
+        self.assertEqual(vertical, 1)
+        image, horizontal, vertical = self.image_grid.validate_image_size(self.oversize_y)
         image.crop.assert_called_once_with((0, 1, 16, 10,))
-        image = self.image_grid.validate_image_size(self.oversize_xy)
+        self.assertEqual(horizontal, 1)
+        self.assertEqual(vertical, 1)
+        image, horizontal, vertical = self.image_grid.validate_image_size(self.oversize_xy)
         image.crop.assert_called_once_with((1, 1, 17, 10,))
-        image = self.image_grid.validate_image_size(self.oversize_xyr)
+        self.assertEqual(horizontal, 1)
+        self.assertEqual(vertical, 1)
+        image, horizontal, vertical = self.image_grid.validate_image_size(self.oversize_xyr)
         image.crop.assert_called_once_with((0, 0, 16, 9,))
-        image = self.image_grid.validate_image_size(self.kingsize_x)
+        self.assertEqual(horizontal, 1)
+        self.assertEqual(vertical, 1)
+        image, horizontal, vertical = self.image_grid.validate_image_size(self.kingsize_x)
         image.crop.assert_called_once_with((0, 1, 32, 10,))
-        image = self.image_grid.validate_image_size(self.kingsize_y)
+        self.assertEqual(horizontal, 2)
+        self.assertEqual(vertical, 1)
+        image, horizontal, vertical = self.image_grid.validate_image_size(self.kingsize_y)
         image.crop.assert_called_once_with((0, 1, 16, 19,))
+        self.assertEqual(horizontal, 1)
+        self.assertEqual(vertical, 2)
+        try:
+            self.image_grid.validate_image_size(self.too_small_x)
+            self.fail("Grid didn't reject too narrow image")
+        except ImageRejected:
+            pass
+        try:
+            self.image_grid.validate_image_size(self.too_small_y)
+            self.fail("Grid didn't reject too short image")
+        except ImageRejected:
+            pass
+        try:
+            self.image_grid.validate_image_size(self.too_small_xy)
+            self.fail("Grid didn't reject too narrow and too short image")
+        except ImageRejected:
+            pass
+
+
