c47161544dfd08a89dfc31ccf7271a54ddb96de0
fako <email@fakoberkers.nl>
2015-08-21 11:54:16 +0200

    Start with another approach for putting images into the grid.

--- core/utils/image.py
+++ core/utils/image.py
@@ -36,6 +36,9 @@ class ImageGrid(object):
         secondary_dimension *= new_size / primary_dimension
         return new_size, int(round(secondary_dimension))
 
+    def get_image_info(self, image):
+        pass
+
     def center_image(self, image, delta_width, delta_height, horizontal, vertical):
         assert isinstance(image, Image.Image), "Center image can only be done with a PIL image"
         assert delta_width >= 0, "Can't center an image if its delta_width is {}".format(delta_width)
@@ -57,12 +60,22 @@ class ImageGrid(object):
         image.crop(box)
         return image, horizontal, vertical
 
-    def validate_image_size(self, image, horizontal=1, vertical=1):
-        # Validate grid (TODO: move)
-        if horizontal > self.columns:
-            raise NoCellAvailable("Image spans more columns than available")
-        if vertical > self.rows:
-            raise NoCellAvailable("Image spans more rows than available")
+    def validate_image_size(self, image):
+        image_width, image_height = image.size
+        if image_width < self.cell_width or image_height < self.cell_height:
+            raise ImageRejected("Image with size {}:{} is too small for cell size {}:{}".format(
+                image_width,
+                image_height,
+                self.cell_width,
+                self.cell_height
+            ))
+
+    def old(self, image, horizontal, vertical):
+        # # Validate grid (TODO: move)
+        # if horizontal > self.columns:
+        #     raise NoCellAvailable("Image spans more columns than available")
+        # if vertical > self.rows:
+        #     raise NoCellAvailable("Image spans more rows than available")
 
         # Calculate image properties
         image_width, image_height = image.size
@@ -105,12 +118,42 @@ class ImageGrid(object):
 
         raise AssertionError("validate_image_size didn't process its image at all")
 
-    def cell_image(self, image):
+    def size_image(self, image):
         assert isinstance(image, Image.Image), "Cell image expects a PIL image not {}".format(type(image))
-        free_cell_index = next([cell for cell in self.cells if cell])
+        self.validate_image_size(image)
+        is_landscape, is_panorama, is_portrait = self.get_image_info(image)
 
-        image, horizontal, vertical = self.validate_image_size(image)  # could raise ImageRejected
-        for cell_index_modifier in xrange(0, horizontal*self.columns, self.columns):
+        if is_landscape:
+            # Resize to cell height with aspect ratio
+            # Center image horizontally
+            horizontal, vertical = 1, 1
+        elif is_panorama:
+            # Resize to cell width with aspect ratio
+            # Center image horizontally
+            horizontal, vertical = 2, 1
+            pass
+        elif is_portrait:
+            # Resize to cell width with aspect ratio
+            # Center image vertically
+            horizontal, vertical = 1, 2
+            pass
+        else:
+            raise AssertionError("Image wasn't labeled as any category")
+
+        return image, horizontal, vertical
+
+    def cell_image(self, image):
+        free_cell_index = next([index for index, cell in enumerate(self.cells) if cell])
+
+        image, horizontal, vertical = self.size_image(image)  # could raise ImageRejected
+
+        # Reserve cells for landscape and panorama
+        for cell_index_modifier in xrange(0, horizontal):
+            cell_index = free_cell_index + cell_index_modifier
+            self.cells[cell_index] = image
+
+        # Reserve cells for portrait
+        for cell_index_modifier in xrange(0, vertical*self.columns, self.columns):
             cell_index = free_cell_index + cell_index_modifier
             self.cells[cell_index] = image
 
