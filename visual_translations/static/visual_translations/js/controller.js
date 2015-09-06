704e414877a7213bcc96b737900f4711de4592c7
fako <email@fakoberkers.nl>
2015-09-06 11:58:40 +0200

    Tidy the code a bit.

---
+++ visual_translations/static/visual_translations/js/controller.js
@@ -0,0 +1,45 @@
+$(function(){
+
+	var rad2deg = 180/Math.PI;
+	var deg = 0;
+	var bars = $('#bars');
+
+	// Place the colorbars
+    $('.colorBar').each(function(i, el) {
+        deg = i*60;
+        $(el).css({
+            transform: 'rotate(' + (deg - 90) + 'deg)',
+            top: -Math.sin(deg / rad2deg) * 80 + 100,
+            left: Math.cos((180 - deg) / rad2deg) * 80 + 75
+        });
+    });
+
+	var colorBars = bars.find('.colorBar');
+	var numBars = 0, lastNum = -1;
+
+	$('#control').knobKnob({
+		snap : 10,
+		value: 154,
+		turn : function(ratio) {
+
+			numBars = Math.round(ratio/(1/6));
+
+			// Update the dom only when the number of active bars
+			// changes, instead of on every move
+			if(numBars == lastNum){
+				return false;
+			}
+			lastNum = numBars;
+
+            $('.colorBar').each(function (i, el) {
+                var $el = $(el);
+                if(i === numBars || (numBars === 6 && i === 0)) {
+                    $el.addClass('active');
+                } else {
+                    $el.removeClass('active');
+                }
+            });
+		}
+	});
+
+});
