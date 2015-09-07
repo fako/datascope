5a1f3e48cde28fc7ec10140a46d3c9c11b25163c
fako <email@fakoberkers.nl>
2015-09-07 16:19:04 +0200

    Working version with websockets that send messages to redis.

--- visual_translations/urls.py
+++ visual_translations/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 
 from core.views.community import CommunityView
 from visual_translations.models import VisualTranslationsCommunity
-from visual_translations.views import (VisualTranslationsHtmlView, info, visual_translation_map,
+from visual_translations.views import (VisualTranslationsHtmlView, visual_translation_map,
                                        visual_translations_controller, web_sockets_broadcast)
 
 
@@ -13,7 +13,6 @@ urlpatterns = [
         {"community_class": VisualTranslationsCommunity}),
     url(r'^visual-translations/html/(?P<path>.+)?/?$', VisualTranslationsHtmlView.as_view(),
         {"community_class": VisualTranslationsCommunity}),
-    url(r'^visual-translations/info/?$', info),
     url(r'^visual-translations/map/(?P<region>[a-z]+)/(?P<term>[a-z+]+)/?$', visual_translation_map),
     url(r'^visual-translations/controller/$', visual_translations_controller),
     url(r'^visual-translations/broadcast/(?P<message>[a-z\-]+)/?$', web_sockets_broadcast),
