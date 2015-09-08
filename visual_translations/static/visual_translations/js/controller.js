09bacbafad18846c3428ef299b3613dd895d1914
fako <email@fakoberkers.nl>
2015-09-08 18:12:02 +0200

    Add arrows.

--- visual_translations/static/visual_translations/js/controller.js
+++ visual_translations/static/visual_translations/js/controller.js
@@ -7,7 +7,7 @@ $(function(){
     var $knobLabels = $knobComponent.find('.knobLabel');
     var $control = $('#control');
 
-	// Place the colorbars
+	// Place the knob labels
     $knobLabels.each(function(i, el) {
         deg = i*60;
         $el = $(el);
@@ -21,6 +21,21 @@ $(function(){
         })
     });
 
+    // Place the arrows
+    var $arrowsComponent = $('#arrows-component');
+    var $arrows = $arrowsComponent.find('.arrow');
+    $arrows.each(function(i, el) {
+        deg = i*45;
+        $el = $(el);
+        $el.css({
+            transform: 'rotate(' + (deg - 90) + 'deg)',
+            top: -Math.sin(deg / rad2deg) * 80 + 100,
+            left: Math.cos((180 - deg) / rad2deg) * 80 + 75
+        });
+        $el.click(function(e) {
+            console.log("clicked: ", $arrows.index($(this)));
+        })
+    });
 
 	var numBars = 0, lastNum = -1;
 
@@ -47,5 +62,7 @@ $(function(){
             });
 		}
 	});
+
+    $control.knob('snapTo', 0);
 
 });
