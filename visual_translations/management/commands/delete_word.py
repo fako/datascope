9b194e0c8d8654e4d09f22b9506928981ef15848
fako <email@fakoberkers.nl>
2015-09-10 12:57:13 +0200

    Adjust plain HTML view and provide commands for deleting words/images.

---
+++ visual_translations/management/commands/delete_word.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+from core.management.commands._community import CommunityCommand
+from core.utils.configuration import DecodeConfigAction
+
+
+class Command(CommunityCommand):
+
+    community_model = "VisualTranslationsCommunity"
+
+    def add_arguments(self, parser):
+        parser.add_argument('community', type=unicode, nargs="?", default=self.community_model)
+        parser.add_argument('-a', '--args', type=unicode, nargs="*", default="")
+        parser.add_argument('-c', '--config', type=unicode, action=DecodeConfigAction, nargs="?", default={})
+        parser.add_argument('-l', '--locale', type=unicode, nargs="?", default="")
+        parser.add_argument('-w', '--word', type=unicode, nargs="?", default="")
+
+    def handle_community(self, community, **options):
+        qs = community.individual_set.filter(properties__contains=options["locale"]).filter(properties__contains="word\": \"{}\"".format(options["word"]))
+        import ipdb; ipdb.set_trace()
+        print("Deleting {} individuals".format(qs.count()))
+        for ind in qs:
+            print(ind.properties)
--- visual_translations/views.py
+++ visual_translations/views.py
@@ -6,6 +6,7 @@ from ws4redis.publisher import RedisPublisher
 from ws4redis.redis_store import RedisMessage
 
 from core.views.community import HtmlCommunityView
+
 from visual_translations.models import VisualTranslationsCommunity
 
 
@@ -15,9 +16,9 @@ class VisualTranslationsHtmlView(HtmlCommunityView):
     def data_for(community_class, response):
         data = HtmlCommunityView.data_for(community_class, response)
         if response.status_code == 200:
-            lang_key = lambda obj: obj["language"]
-            data["results"].sort(key=lang_key)
-            response.data["results"] = [(lang, list(words),) for lang, words in groupby(data["results"], lang_key)]
+            locale_key = lambda obj: obj["locale"]
+            data["results"].sort(key=locale_key)
+            response.data["results"] = [(lang, list(words),) for lang, words in groupby(data["results"], locale_key)]
             return response.data
         else:
             return response.data
