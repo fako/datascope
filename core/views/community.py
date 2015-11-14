dfa98bd8281f60137144dcdaafe524bc3e46443f
fako <email@fakoberkers.nl>
2015-11-14 15:42:57 +0100

    Update view to work with Manifestation when available.

--- core/views/community.py
+++ core/views/community.py
@@ -11,7 +11,7 @@ from rest_framework.response import Response
 from rest_framework.status import (HTTP_200_OK, HTTP_202_ACCEPTED, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST,
                                    HTTP_500_INTERNAL_SERVER_ERROR)
 
-from core.models.organisms.community import CommunityState
+from core.models.organisms.community import CommunityState, Manifestation
 from core.exceptions import DSProcessUnfinished, DSProcessError
 
 
@@ -28,31 +28,50 @@ class CommunityView(APIView):
         "error": None
     }
 
-    def get(self, request, community_class, path="", *args, **kwargs):
-        response_data = copy(self.RESPONSE_DATA)
-        community, created = community_class.get_or_create_by_input(*path.split('/'), **request.GET.dict())
+    def get_response_from_manifestation(self, manifestation, response_data):
+        manifestation_data = manifestation.get_data()
+        if not manifestation_data:
+            return Response(None, HTTP_204_NO_CONTENT)
+        results_key = "results" if isinstance(manifestation_data, list) else "result"
+        response_data[results_key] = manifestation_data
+        return Response(response_data, HTTP_200_OK)
 
-        if community.state == CommunityState.SYNC:
-            # FEATURE: set status
-            return Response(response_data, HTTP_202_ACCEPTED)
+    def get(self, request, community_class, path="", *args, **kwargs):
+
+        response_data = copy(self.RESPONSE_DATA)
+        full_path = request.get_full_path()
         try:
+            manifestation = Manifestation.objects.get(uri=full_path)
+            community = manifestation.community
+        except Manifestation.DoesNotExist:
+            manifestation = None
+            community, created = community_class.get_or_create_by_input(
+                *path.split('/'),
+                **request.GET.dict()
+            )
+
+        try:
+
+            if manifestation is not None:
+                return self.get_response_from_manifestation(manifestation, response_data)
+            if community.state == CommunityState.SYNC:
+                raise DSProcessUnfinished()
+
             community.grow(*path.split('/'))
+            manifestation = Manifestation.objects.create(uri=full_path, community=community)
+            return self.get_response_from_manifestation(manifestation, response_data)
+
         except ValidationError as exc:
             response_data["error"] = exc
             return Response(response_data, HTTP_400_BAD_REQUEST)
+
         except DSProcessUnfinished:
             # FEATURE: set status
             return Response(response_data, HTTP_202_ACCEPTED)
+
         except DSProcessError:
             # FEATURE: set errors
             return Response(response_data, HTTP_500_INTERNAL_SERVER_ERROR)
-
-        manifestation = community.manifestation
-        if not manifestation:
-            return Response(None, HTTP_204_NO_CONTENT)
-        results_key = "results" if isinstance(manifestation, list) else "result"
-        response_data[results_key] = manifestation
-        return Response(response_data, HTTP_200_OK)
 
     # FEATURE: allow actions who's function lives on a Community through POST
     # def post(self, request, action, community_class, *args, **kwargs):
