4dcfa988d1ac68d25031630bbfac4d9564297279
Fako Berkers <email@fakoberkers.nl>
2016-02-09 08:37:28 +0100

    Plan to work with merge sort to handle ranking.

--- core/processors/rank.py
+++ core/processors/rank.py
@@ -26,6 +26,15 @@ class RankProcessor(object):
             for hook in six.iterkeys(config_dict)  # config gets whitelisted by Community
             if isinstance(hook, str) and hook.startswith("$") and callable(getattr(self, hook[1:], None))
         ]
+        # TODO: There are problems with the memory consumption of this piece of code.
+        # 1)   Give the kernel to "body" processor methods instead of the content. Make a default content reader processor
+        # 2)   Make the content of collectives return a generator (use ( and ) instead of [ and ])
+        # 3)   Use content for each hook to calculate the total (using reduce?) and write batches to numbered batch files in a folder with name: rank-<kernal_id>-<hooks>
+        # 4)   If such a folder exists raise DuplicateProcessorInAction() and catch to return 202
+        # 5)   Write "ranked" temp files that have ds_rank set
+        # 6)   Read all ranked hook files as batches and combine the ratings in new tmp files that are sorted
+        # 7)   Return an iterator over all combined files using heapq.merge
+        # 8)   Make manifestations always work with iterators and use itertools.islice for wiki_news
         for hook in hooks:
             total = 0
             for individual in individuals:
