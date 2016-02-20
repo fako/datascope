198ed51b144895d223972f3896d588e3153b12f6
Fako Berkers <email@fakoberkers.nl>
2016-02-20 16:07:39 +0100

    Change MySQL setup for digital ocean and others.

--- datascope/settings/digital-ocean.py
+++ datascope/settings/digital-ocean.py
@@ -4,4 +4,3 @@ DATABASES["default"]["ENGINE"] = 'django.db.backends.mysql'
 DATABASES["default"]["NAME"] = 'datascope'
 DATABASES["default"]["USER"] = 'django'
 DATABASES["default"]["PASSWORD"] = MYSQL_PASSWORD
-
--- datascope/settings/wikipedia.py
+++ datascope/settings/wikipedia.py
@@ -3,5 +3,5 @@ from base import *
 DATABASES["default"]["ENGINE"] = 'django.db.backends.mysql'
 DATABASES["default"]["NAME"] = 's52573__datascope'
 DATABASES["default"]["USER"] = 's52573'
-DATABASES["default"]["PASSWORD"] = WIKILABS_MYSQL_PASSWORD
+DATABASES["default"]["PASSWORD"] = MYSQL_PASSWORD
 DATABASES["default"]["HOST"] = 'tools-db'
