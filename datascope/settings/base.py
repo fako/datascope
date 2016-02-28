ac05df903a2b20f4b9b815c2fcb8c0beca4feb0f
Fako Berkers <email@fakoberkers.nl>
2016-02-28 19:17:43 +0000

    Fix statics for Wikipedia deploy.

--- datascope/settings/base.py
+++ datascope/settings/base.py
@@ -116,7 +116,7 @@ MEDIA_ROOT = PATH_TO_PROJECT + 'system/files/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = URL_TO_PROJECT + 'media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -126,7 +126,7 @@ STATIC_ROOT = PATH_TO_PROJECT + 'system/files/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = URL_TO_PROJECT + 'static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
