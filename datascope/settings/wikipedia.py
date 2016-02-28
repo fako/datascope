b668ee6e4b2103eb145e8240f13a80d38ac0d846
Fako Berkers <email@fakoberkers.nl>
2016-02-28 19:36:04 +0000

    Set static URL for Wikipedia based installations.

--- datascope/settings/wikipedia.py
+++ datascope/settings/wikipedia.py
@@ -6,4 +6,6 @@ DATABASES["default"]["USER"] = 's52573'
 DATABASES["default"]["PASSWORD"] = MYSQL_PASSWORD
 DATABASES["default"]["HOST"] = 'tools-db'
 
+STATIC_URL = "http://tools-static.wmflabs.org/algo-news/static/"
+
 # TODO: add STATIC_IP
