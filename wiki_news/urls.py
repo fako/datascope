5df883b3d79299ad562cdf6d7fa6d9d1738dd0e0
Fako Berkers <email@fakoberkers.nl>
2016-02-29 00:06:42 +0000

    Make community views more generic in order to handle responses without having exact requests.

--- wiki_news/urls.py
+++ wiki_news/urls.py
@@ -4,9 +4,21 @@ from django.conf.urls import url
 
 from core.views.community import CommunityView, HtmlCommunityView
 from wiki_news.models import WikiNewsCommunity
+from . import views
 
 
 urlpatterns = [
-    url(r'^wiki-algo-news/service/(?P<path>.+)/$', CommunityView.as_view(), {"community_class": WikiNewsCommunity}),
-    url(r'^wiki-algo-news/html/(?P<path>.+)/$', HtmlCommunityView.as_view(), {"community_class": WikiNewsCommunity})
+    url(
+        r'^wiki-algo-news/service/(?P<path>.+)/$',
+        CommunityView.as_view(),
+        kwargs={"community_class": WikiNewsCommunity},
+        name=WikiNewsCommunity.get_name() + "_service"
+    ),
+    url(
+        r'^wiki-algo-news/html/(?P<path>.+)/$',
+        HtmlCommunityView.as_view(),
+        kwargs={"community_class": WikiNewsCommunity},
+        name=WikiNewsCommunity.get_name() + "_html"
+    ),
+    url(r'^wiki-algo-news/wiki-update/(?P<page>.+)/$', views.wiki_page_update, name="wiki_page_update")
 ]
