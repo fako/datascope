99b8c1bd76ef12312259be9ddbdac644d764b57e
Fako Berkers <email@fakoberkers.nl>
2016-03-01 15:21:45 +0000

    Add scripts that show how to run Django commands on Wikipedia Tools.

---
+++ system/scripts/run_wiki_celery.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# NB: Use jstart -l release=trusty run_wiki_celery.sh to make this script run a celery worker on Tools
+# NB: Use job -v run_wiki_celery to see status of the worker
+
+source /data/project/algo-news/env/bin/activate
+cd /data/project/algo-news/src/
+export DJANGO_SETTINGS_MODULE=datascope.settings.wikipedia
+python manage.py celery worker -l info
