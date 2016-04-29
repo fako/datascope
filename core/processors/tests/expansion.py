12bfa9b5fb251bbf8abee70e7ea7c4f6ebc48c86
Fako Berkers <email@fakoberkers.nl>
2016-04-29 21:08:56 +0200

    Add missing tests to be filled out later.

---
+++ core/processors/tests/expansion.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+from django.test import TestCase
+
+
+class TestExpansionProcessor(TestCase):
+
+    def test_collective_content(self):
+        self.skipTest("not tested")
---
+++ core/utils/tests/helpers.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+from unittest import TestCase
+
+
+class TestUtilHelpers(TestCase):
+
+    def test_get_any_model(self):
+        self.skipTest("not tested")
+
+    def test_dict_override(self):
+        self.skipTest("not tested")
+
+    def test_merge_iter(self):
+        self.skipTest("not tested")
