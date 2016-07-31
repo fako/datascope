4ce77a2ecc5babd1479dbec33b3e556b3004704f
Fako Berkers <email@fakoberkers.nl>
2016-07-31 15:27:57 +0200

    Allow installation to run without scipy

--- sources/processors/indico/compare.py
+++ sources/processors/indico/compare.py
@@ -1,7 +1,5 @@
 from __future__ import unicode_literals, absolute_import, print_function, division
 
-from scipy.spatial.distance import euclidean
-
 from core.processors.compare import ComparisonProcessor
 
 
@@ -9,6 +7,7 @@ class ImageFeaturesCompareProcessor(ComparisonProcessor):
 
     @staticmethod
     def euclidean_distance(individual, reference_individual):
+        from scipy.spatial.distance import euclidean
         distance = euclidean(individual["vectors"], reference_individual["vectors"])
         if not distance:
             return 0.99999999
