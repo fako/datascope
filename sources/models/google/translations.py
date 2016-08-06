3e8f7402b151f0fdba4dcd42c10317badf8ac61f
Fako Berkers <email@fakoberkers.nl>
2016-08-06 11:07:05 +0200

    Improve fallback of GoogleTranslate.

--- sources/models/google/translations.py
+++ sources/models/google/translations.py
@@ -22,26 +22,27 @@ class GoogleTranslate(BrowserResource):
         confidences = soup.find_all(class_="gt-baf-cts")
         words = soup.find_all(class_="gt-baf-word-clickable")
         meanings = soup.find_all(class_="gt-baf-translations")
-        try:
-            fallback = next(word for word in soup.find_all("span") if word.parent.get("id") == "result_box")
-        except StopIteration:
-            fallback = None
-            log.error("No fallback for: {}".format(self.uri))
 
         def process_info(word, meaning, confidence):
-            if word is not None:
-                word = word.text
-                meaning = meaning.text
-            elif fallback is not None:
-                word = str(fallback)
-                meaning = ""
-            else:
+            if word is None:
                 raise DSNoContent("GoogleTranslate could not find any content for: {}".format(self.uri))
+            word = word.text
+            if meaning is not None:
+                meaning = meaning.text
             if confidence is not None:
                 confidence = int(confidence["style"].split(" ")[1][:-3])  # confidence expressed like: "width: 24px;"
             return word, meaning, confidence
 
+        if not words:
+            try:
+                words = [
+                    next(word for word in soup.find_all("span") if word.parent.get("id") == "result_box")
+                ]
+            except StopIteration:
+                log.error("No fallback for: {}".format(self.uri))
+
         info = zip_longest(words, meanings, confidences)
+
         return [
             {
                 "language": self.request["args"][1],
