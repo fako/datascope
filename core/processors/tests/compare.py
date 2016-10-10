10a41bd006cffb212abf8c8e8f0daec4b78a7950
Fako Berkers <email@fakoberkers.nl>
2016-10-10 21:24:18 +0200

    Add tests for compare and expand processors

---
+++ core/processors/tests/compare.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+from django.test import TestCase
+
+
+class TestCompareProcessor(TestCase):
+
+    def test_get_hook_arguments(self):
+        self.skipTest("not tested")
--- core/tests/tests.py
+++ core/tests/tests.py
@@ -6,6 +6,8 @@ from core.processors.tests.resources import (TestHttpResourceProcessor, TestHttp
                                              TestHttpResourceProcessorPost)
 from core.processors.tests.extraction import TestExtractProcessor
 from core.processors.tests.rank import TestRankProcessor
+from core.processors.tests.expansion import TestExpansionProcessor
+from core.processors.tests.compare import TestCompareProcessor
 
 from core.models.organisms.tests.growth import TestGrowth
 from core.models.organisms.tests.community import TestCommunityMock
