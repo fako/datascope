72591be26aa46e7ca720f5beacef55368cc5e448
Fako Berkers <email@fakoberkers.nl>
2016-10-10 17:51:51 +0200

    Check that identifier gets updated correctly on Collective.

--- core/models/organisms/tests/growth.py
+++ core/models/organisms/tests/growth.py
@@ -303,7 +303,7 @@ class TestGrowth(TestProcessorMixin):
         contributions = self.collective_input.prepare_contributions(qs)
         self.collective_input.inline_by_key(contributions, "value")
         self.assertEqual(list(self.collective_input.output.content), self.expected_inline_output)
-        self.skipTest("test clean and correction of the identifier")
+        self.assertEqual(self.collective_input.output.identifier, "value.value")
 
     def test_update_by_key(self):
         self.skipTest("not tested")
