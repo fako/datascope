cbe72c066c79f98a6a6422283a58d6a6ebcc493b
Fako Berkers <email@fakoberkers.nl>
2016-10-14 13:29:23 +0200

    Change test code a bit in structure.

--- core/processors/tests/expansion.py
+++ core/processors/tests/expansion.py
@@ -9,38 +9,52 @@ from core.processors import ExpansionProcessor
 class TestExpansionProcessor(TestCase):
 
     fixtures = ["test-organisms"]
-    expected_content_expansion = {
-        "individual": "content",
-        "expand": [
-            {
-                "value": "nested value {}".format(index),
-                "context": "nested value"
-            }
-            for index in range(3)
-        ]
-    }
-    expected_instance_expansion = {
-        "individual": "instance",
-        "expand": [
-            {
-                "value": "nested value {}".format(index),
-                "context": "nested value"
-            }
-            for index in range(3)
-        ]
-    }
 
-    def test_collective_content(self):
-        individual_content = {
+    def setUp(self):
+        self.individual_content = {
             "individual": "content",
             "expand": "/data/v1/collective/1/content/"
         }
-        individual = Individual(properties={
+        self.individual = Individual(properties={
             "individual": "instance",
             "expand": "/data/v1/collective/1/"
         })
+        self.expected_content_expansion = {
+            "individual": "content",
+            "expand": [
+                {
+                    "value": "nested value {}".format(index),
+                    "context": "nested value"
+                }
+                for index in range(3)
+            ]
+        }
+        self.expected_instance_expansion = {
+            "individual": "instance",
+            "expand": [
+                {
+                    "value": "nested value {}".format(index),
+                    "context": "nested value"
+                }
+                for index in range(3)
+            ]
+        }
+
+    def test_collective_content_list(self):
         processor = ExpansionProcessor(config={})
-        generator = processor.collective_content([individual_content, individual])
+        generator = processor.collective_content([self.individual_content, self.individual])
+        test_content, test_individual = list(generator)
+        self.assertIsInstance(test_content, dict)
+        self.assertEqual(test_content, self.expected_content_expansion)
+        self.assertIsInstance(test_individual, Individual)
+        self.assertEqual(test_individual.properties, self.expected_instance_expansion)
+
+    def test_collective_content_iterator(self):
+
+        processor = ExpansionProcessor(config={})
+        generator = processor.collective_content(
+            iter([self.individual_content, self.individual])
+        )
         test_content, test_individual = list(generator)
         self.assertIsInstance(test_content, dict)
         self.assertEqual(test_content, self.expected_content_expansion)
