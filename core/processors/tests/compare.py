d08d361c0a982ce4c532c979b8c2434062e1e316
Fako Berkers <email@fakoberkers.nl>
2016-10-19 11:57:04 +0200

    Add tests for the comparison processor.

--- core/processors/tests/compare.py
+++ core/processors/tests/compare.py
@@ -2,8 +2,36 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 
 from django.test import TestCase
 
+from core.processors.compare import ComparisonProcessor
+from core.models import Individual
+
 
 class TestCompareProcessor(TestCase):
 
+    fixtures = ["test-organisms"]
+
+    def setUp(self):
+        self.prc = ComparisonProcessor(config={"reference": "1"})
+
+    def test_init(self):
+        prc = ComparisonProcessor(config={"reference": 1})
+        self.assertIsInstance(prc, ComparisonProcessor)
+        try:
+            ComparisonProcessor(config={"reference": "100"})
+            self.fail("CompareProccessor should not init with non-existing references")
+        except Individual.DoesNotExist:
+            pass
+        try:
+            ComparisonProcessor(config={"reference": "bull"})
+            self.fail("CompareProccessor should not init with non-id values for reference")
+        except ValueError:
+            pass
+
     def test_get_hook_arguments(self):
-        self.skipTest("not tested")
+        ind = Individual.objects.get(id=2)
+        args = self.prc.get_hook_arguments(ind.properties)
+        self.assertEqual(args, (ind.properties, self.prc.reference.properties))
+        ind.properties["context"] = "changed"
+        self.prc.reference.properties["context"] = "changed"
+        for arg in args:
+            self.assertEqual(arg["context"], "nested value")
