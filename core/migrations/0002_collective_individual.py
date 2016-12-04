bb7c9bcefb8f5fae127a80ed0d3c27d77a2546b6
Fako Berkers <email@fakoberkers.nl>
2016-12-04 10:50:20 +0100

    Fix problems with legacy packages used in migrations.

--- core/migrations/0002_collective_individual.py
+++ core/migrations/0002_collective_individual.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import jsonfield.fields
+import json_field as jsonfield
 
 
 class Migration(migrations.Migration):
