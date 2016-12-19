0f6124b74a2c8cbf89c01bb26c9061386a86a3b3
Fako Berkers <email@fakoberkers.nl>
2016-12-20 00:03:46 +0100

    Add migration that changes verbose names of some Wikipedia resources.

---
+++ sources/migrations/0016_auto_20161220_0001.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('sources', '0015_wikipediacategories_wikipediacategorymembers'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='wikipediacategories',
+            options={'verbose_name_plural': 'Wikipedia categories', 'verbose_name': 'Wikipedia category'},
+        ),
+        migrations.AlterModelOptions(
+            name='wikipediacategorymembers',
+            options={'verbose_name_plural': 'Wikipedia category members', 'verbose_name': 'Wikipedia category members'},
+        ),
+        migrations.AlterModelOptions(
+            name='wikipediasearch',
+            options={'verbose_name_plural': 'Wikipedia searches', 'verbose_name': 'Wikipedia search'},
+        ),
+    ]
