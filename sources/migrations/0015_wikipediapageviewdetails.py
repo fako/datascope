da00c9cce11d12970242c6c0abcd128901b10ae5
Fako Berkers <email@fakoberkers.nl>
2017-03-03 19:01:49 +0100

    Resolves merge conflict for migrations with master branch

--- sources/migrations/0015_wikipediapageviewdetails.py
+++ sources/migrations/0015_wikipediapageviewdetails.py
@@ -2,27 +2,27 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import json_field.fields
 import core.utils.configuration
+import json_field.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('contenttypes', '0002_remove_content_type_name'),
-        ('sources', '0013_imagefeatures'),
+        ('sources', '0014_wikipediacategories_wikipediacategorymembers'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='WikipediaPageviewDetails',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
-                ('uri', models.CharField(db_index=True, max_length=255, default=None)),
-                ('data_hash', models.CharField(db_index=True, max_length=255, default='')),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('uri', models.CharField(db_index=True, default=None, max_length=255)),
+                ('data_hash', models.CharField(db_index=True, default='', max_length=255)),
                 ('config', core.utils.configuration.ConfigurationField()),
-                ('request', json_field.fields.JSONField(help_text='Enter a valid JSON object', default=None)),
-                ('head', json_field.fields.JSONField(help_text='Enter a valid JSON object', default=None)),
+                ('request', json_field.fields.JSONField(default=None, help_text='Enter a valid JSON object')),
+                ('head', json_field.fields.JSONField(default=None, help_text='Enter a valid JSON object')),
                 ('body', models.TextField(default=None)),
                 ('status', models.PositiveIntegerField(default=None)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
