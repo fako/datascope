1b7c3894b95cc1a76ef8519ea2cc0c276be6bfac
Fako Berkers <email@fakoberkers.nl>
2017-03-03 18:54:23 +0100

    Recreates the wikipedia category resources

--- sources/migrations/0014_wikipediacategories_wikipediacategorymembers.py
+++ sources/migrations/0014_wikipediacategories_wikipediacategorymembers.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import core.utils.configuration
 import json_field.fields
+import core.utils.configuration
 
 
 class Migration(migrations.Migration):
@@ -17,49 +17,43 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='WikipediaCategories',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
                 ('uri', models.CharField(db_index=True, default=None, max_length=255)),
                 ('data_hash', models.CharField(db_index=True, default='', max_length=255)),
                 ('config', core.utils.configuration.ConfigurationField()),
-                ('request', json_field.fields.JSONField(default=None, help_text='Enter a valid JSON object')),
-                ('head', json_field.fields.JSONField(default=None, help_text='Enter a valid JSON object')),
+                ('request', json_field.fields.JSONField(help_text='Enter a valid JSON object', default=None)),
+                ('head', json_field.fields.JSONField(help_text='Enter a valid JSON object', default=None)),
                 ('body', models.TextField(default=None)),
                 ('status', models.PositiveIntegerField(default=None)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('modified_at', models.DateTimeField(auto_now=True)),
                 ('purge_at', models.DateTimeField(blank=True, null=True)),
                 ('retainer_id', models.PositiveIntegerField(null=True)),
-                ('retainer_type', models.ForeignKey(null=True, to='contenttypes.ContentType')),
+                ('retainer_type', models.ForeignKey(to='contenttypes.ContentType', null=True)),
             ],
             options={
-                'verbose_name': 'Wikipedia category',
-                'verbose_name_plural': 'Wikipedia categories',
+                'abstract': False,
             },
         ),
         migrations.CreateModel(
             name='WikipediaCategoryMembers',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
                 ('uri', models.CharField(db_index=True, default=None, max_length=255)),
                 ('data_hash', models.CharField(db_index=True, default='', max_length=255)),
                 ('config', core.utils.configuration.ConfigurationField()),
-                ('request', json_field.fields.JSONField(default=None, help_text='Enter a valid JSON object')),
-                ('head', json_field.fields.JSONField(default=None, help_text='Enter a valid JSON object')),
+                ('request', json_field.fields.JSONField(help_text='Enter a valid JSON object', default=None)),
+                ('head', json_field.fields.JSONField(help_text='Enter a valid JSON object', default=None)),
                 ('body', models.TextField(default=None)),
                 ('status', models.PositiveIntegerField(default=None)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('modified_at', models.DateTimeField(auto_now=True)),
                 ('purge_at', models.DateTimeField(blank=True, null=True)),
                 ('retainer_id', models.PositiveIntegerField(null=True)),
-                ('retainer_type', models.ForeignKey(null=True, to='contenttypes.ContentType')),
+                ('retainer_type', models.ForeignKey(to='contenttypes.ContentType', null=True)),
             ],
             options={
-                'verbose_name': 'Wikipedia category members',
-                'verbose_name_plural': 'Wikipedia category members',
+                'abstract': False,
             },
-        ),
-        migrations.AlterModelOptions(
-            name='wikipediasearch',
-            options={'verbose_name': 'Wikipedia search', 'verbose_name_plural': 'Wikipedia searches'},
         ),
     ]
