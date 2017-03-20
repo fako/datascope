dc3064e3d2f1250b568a7854310cb1a55382d4c6
Fako Berkers <email@fakoberkers.nl>
2017-03-20 15:25:00 +0100

    Makes the tests run without migrations which makes them much faster.

--- datascope/settings/test.py
+++ datascope/settings/test.py
@@ -5,3 +5,15 @@ MAX_BATCH_SIZE = None  # better for sqlite to let Django determine batch size
 STATIC_IP = "127.0.0.1"
 
 LOGGING["loggers"] = {}
+
+
+class DisableMigrations(object):
+
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return "notmigrations"
+
+
+MIGRATION_MODULES = DisableMigrations()
