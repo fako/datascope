8327911bb959d6defae37f12c873fe2e1db2ae53
Fako Berkers <email@fakoberkers.nl>
2017-03-22 21:01:28 +0100

    Cleans the implementation of the wikipedia ranking modules.

--- sources/processors/wikipedia/rank.py
+++ sources/processors/wikipedia/rank.py
@@ -1,38 +1,36 @@
-from __future__ import unicode_literals, absolute_import, print_function, division
-import six
-
 from core.processors.rank import RankProcessor
 
 
 class WikipediaRankProcessor(RankProcessor):
 
+    def get_hook_arguments(self, individual):
+        individual_argument = super(WikipediaRankProcessor, self).get_hook_arguments(individual)[0]
+        wikidata_argument = individual_argument.get("wikidata", {})
+        if wikidata_argument is None or isinstance(wikidata_argument, str):
+            wikidata_argument = {}
+        return [individual_argument, wikidata_argument]
+
     @staticmethod
-    def revision_count(page):
+    def revision_count(page, wikidata):
         return len(page.get("revisions", []))
 
     @staticmethod
-    def category_count(page):
+    def category_count(page, wikidata):
         return len(page.get("categories", []))
 
     @staticmethod
-    def number_of_deaths(page):
-        wikidata = page.get("wikidata")
-        if not wikidata or isinstance(wikidata, six.string_types):
-            return
+    def number_of_deaths(page, wikidata):
         number_of_deaths_property = "P1120"
-        for claim in wikidata.get("claims", []):
-            if claim["property"] == number_of_deaths_property:
-                return claim["value"]
-        return
+        return next(
+            (claim["value"] for claim in wikidata.get("claims", [])
+            if claim["property"] == number_of_deaths_property)
+        , 0)
 
     @staticmethod
-    def women(page):
-        wikidata = page.get("wikidata")
-        if not wikidata or isinstance(wikidata, six.string_types):
-            return
+    def women(page, wikidata):
         sex_property = "P21"
         women_item = "Q6581072"
-        for claim in wikidata.get("claims", []):
-            if claim["property"] == sex_property and claim["value"] == women_item:
-                return True
-        return
+        return any(
+            (claim for claim in wikidata.get("claims", [])
+            if claim["property"] == sex_property and claim["value"] == women_item)
+        )
