3e4402f7c28e9e42c54fc59dfe3204818b897cde
Fako Berkers <email@fakoberkers.nl>
2017-06-09 11:05:26 +0200

    Small improvements for the Locafora command.

--- nautilus/management/commands/grow_locafora.py
+++ nautilus/management/commands/grow_locafora.py
@@ -7,19 +7,27 @@ from pandas import DataFrame
 from core.management.commands.grow_community import Command as GrowCommand
 from core.utils.configuration import DecodeConfigAction
 
+from nautilus.models import LocaforaOrders
+
 
 class Command(GrowCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('community', type=str, nargs="?", default="LocaforaOrderOverviewCommunity")
         parser.add_argument('-c', '--config', type=str, action=DecodeConfigAction, nargs="?", default={})
+        parser.add_argument('-d', '--delete', action="store_true")
 
     def handle_community(self, community, **options):
+        if options["delete"]:
+            LocaforaOrders.objects.all().delete()
         community.signature = datetime.utcnow().strftime(settings.DATASCOPE_DATETIME_FORMAT)
         super(Command, self).handle_community(community, **options)
         result = DataFrame()
         for customer in community.kernel.individual_set.all():
             customer_frame = DataFrame(customer["order_lines"])
             result = result.append(customer_frame, ignore_index=True)
+        if result.empty:
+            print("No orders to process")
+            return
         result.sort("product", inplace=True)
         result.to_csv('out.csv')
