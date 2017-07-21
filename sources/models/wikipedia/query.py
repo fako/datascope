fdd82ce8eb27d5d8112f3879c1cf4a5a7f88680e
Fako Berkers <email@fakoberkers.nl>
2017-07-21 16:50:24 +0200

    Various improvements to the WikipediaQuery class.

--- sources/models/wikipedia/query.py
+++ sources/models/wikipedia/query.py
@@ -1,3 +1,5 @@
+import json
+
 from core.utils.helpers import override_dict
 from core.exceptions import DSInvalidResource, DSHttpError40X
 from sources.models.wikipedia.base import WikipediaAPI
@@ -5,7 +7,7 @@ from sources.models.wikipedia.base import WikipediaAPI
 
 class WikipediaQuery(WikipediaAPI):
 
-    URI_TEMPLATE = 'http://{}.wikipedia.org/w/api.php?{}={}'
+    URI_TEMPLATE = 'https://{}.wikipedia.org/w/api.php?{}={}'
 
     PARAMETERS = override_dict(WikipediaAPI.PARAMETERS, {
         "action": "query",
@@ -34,6 +36,7 @@ class WikipediaQuery(WikipediaAPI):
         response = data['query'][self.WIKI_RESULTS_KEY]  # Wiki has response hidden under single keyed dicts :(
 
         # When searching for pages a dictionary gets returned
+        # TODO: parse the dictionary and possibly return partial content
         if isinstance(response, dict) and "-1" in response and "missing" in response["-1"]:
             self.status = 404
             raise DSHttpError40X(self.ERROR_MESSAGE, resource=self)
@@ -48,6 +51,11 @@ class WikipediaQuery(WikipediaAPI):
         if "warnings" in data:
             del(data["warnings"])
         return content_type, data
+
+    def get_wikipedia_json(self):
+        # TODO: remove this method and its uses when a partial content is possible
+        response = json.loads(self.body)
+        return response["query"][self.WIKI_RESULTS_KEY]
 
     def next_parameters(self):
         content_type, data = self.content
