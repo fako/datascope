0495f0672d88c6ad4f2eae36aeeb094ae8173d76
Fako Berkers <email@fakoberkers.nl>
2017-07-29 21:43:25 +0200

    Fixes problems with celery on wikipedia

--- datascope/settings/wikipedia.py
+++ datascope/settings/wikipedia.py
@@ -9,6 +9,7 @@ DATABASES["default"]["HOST"] = 'tools-db'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 STATIC_URL = "//tools-static.wmflabs.org/algo-news/static/"
 
+CELERY_DEFAULT_QUEUE = 'datascope'
 BROKER_URL = 'redis://tools-redis:6379/0'
 MAX_BATCH_SIZE = 100
 
