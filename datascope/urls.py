281bfad8a69341fb504591e5685a70e3f5bbdbd5
Fako Berkers <email@fakoberkers.nl>
2017-09-09 13:31:02 +0200

    Remove deprecated dotted strings to get to views in urls.py

--- datascope/urls.py
+++ datascope/urls.py
@@ -2,20 +2,22 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 
 from django.conf.urls import include, url
 from django.conf import settings
-
 from django.contrib import admin
+from django.views import static
 admin.autodiscover()
+
+from core.urls import urlpatterns as core_patterns
 
 
 urlpatterns = [
-    url(r'^data/v1/', include('core.urls', namespace="v1")),
+    url(r'^data/v1/', include(core_patterns, namespace="v1")),
     url(r'^admin/', include(admin.site.urls))
 ]
 
 if settings.DEBUG:
     urlpatterns += [
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        url(r'^media/(?P<path>.*)$', static.serve,
             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True }),
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        url(r'^static/(?P<path>.*)$', static.serve,
             {'document_root': settings.STATIC_ROOT })
     ]
