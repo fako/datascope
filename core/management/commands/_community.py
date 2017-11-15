55413fafc5b288c51c44099d0cf0b7b1e527c57f
Fako Berkers <email@fakoberkers.nl>
2017-11-15 12:36:54 +0100

    Cleans 2.7 code from visual_translations.

--- core/management/commands/_community.py
+++ core/management/commands/_community.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals, absolute_import, print_function, division
-
 from datetime import datetime
 import logging
 
--- visual_translations/management/commands/generate_eu_visual_translations.py
+++ visual_translations/management/commands/generate_eu_visual_translations.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals, absolute_import, print_function, division
-
 from core.management.commands.grow_community import Command as GrowCommunityCommand
 
 from sources.models import GoogleImage, GoogleTranslate
