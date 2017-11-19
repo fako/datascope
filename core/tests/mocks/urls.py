fee3fc79f7204cd4893ba496a6f0b75e0563f75a
Fako Berkers <email@fakoberkers.nl>
2017-11-19 15:34:39 +0100

    Adds the mock urls.

---
+++ core/tests/mocks/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+
+from core.views import CommunityView, HtmlCommunityView
+from .community import CommunityMock
+
+
+urlpatterns = [
+    url(
+        r'^mock/service/(?P<path>.+)/$',
+        CommunityView.as_view(),
+        kwargs={"community_class": CommunityMock},
+        name=CommunityMock.get_name() + "_service"
+    ),
+    url(
+        r'^mock/html/(?P<path>.+)?/?$',
+        HtmlCommunityView.as_view(),
+        kwargs={"community_class": CommunityMock},
+        name=CommunityMock.get_name() + "_html"
+    ),
+]
