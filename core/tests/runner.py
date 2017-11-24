76ff6b8df705e6ef0960e98e8358bde9e8406573
Fako Berkers <email@fakoberkers.nl>
2017-11-24 17:07:07 +0100

    Only show messages during tests that are higher than ERROR.

--- core/tests/runner.py
+++ core/tests/runner.py
@@ -5,5 +5,5 @@ from django.test.runner import DiscoverRunner
 
 class DataScopeDiscoverRunner(DiscoverRunner):
     def run_tests(self, test_labels, extra_tests=None, **kwargs):
-        logging.disable(logging.DEBUG)
+        logging.disable(logging.ERROR)
         return super(DataScopeDiscoverRunner, self).run_tests(test_labels, extra_tests, **kwargs)
