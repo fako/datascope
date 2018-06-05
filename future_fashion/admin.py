fc6f9e85ffdbba5b946c47ad6627dfc67aab45ea
Fako Berkers <email@fakoberkers.nl>
2018-06-05 07:18:19 +0200

    Reveals BrandRecognitionService in the admin

--- future_fashion/admin.py
+++ future_fashion/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 from core.admin.organisms import CommunityAdmin
-from future_fashion.models import FutureFashionCommunity, InventoryCommunity
+from core.admin.resources import ResourceAdmin
+from future_fashion.models import FutureFashionCommunity, InventoryCommunity, BrandRecognitionService
 
 
 admin.site.register(FutureFashionCommunity, CommunityAdmin)
 admin.site.register(InventoryCommunity, CommunityAdmin)
+admin.site.register(BrandRecognitionService, ResourceAdmin)
