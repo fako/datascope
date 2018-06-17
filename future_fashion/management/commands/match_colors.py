e7dc72216c82ec5a35f7a0fdb234659d424d7d3e
Fako Berkers <email@fakoberkers.nl>
2018-06-17 10:58:09 +0200

    Refactors color functionality to be useful and consistent across communities

--- future_fashion/management/commands/match_colors.py
+++ future_fashion/management/commands/match_colors.py
@@ -4,34 +4,40 @@ import shutil
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
-from django.core.management.base import BaseCommand
 from django.core.files.storage import default_storage
 
+from core.management.commands import CommunityCommand
+from core.utils.configuration import DecodeConfigAction
 from future_fashion.models import InventoryCommunity
+from future_fashion.colors import get_main_colors_from_file, get_vector_from_colors, get_colors_frame
 
 
-class Command(BaseCommand):
+class Command(CommunityCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('image', type=str)
+        parser.add_argument('community', type=str, nargs="?", default=self.community_model)
+        parser.add_argument('-a', '--args', type=str, nargs="*", default="")
+        parser.add_argument('-c', '--config', type=str, action=DecodeConfigAction, nargs="?", default={})
+        parser.add_argument('-i', '--image', type=str)
 
-    def handle(self, *args, **options):
+    def handle_community(self, community, *args, **options):
+        # Get colors from input file
         image = options["image"]
-        # Get inventory and calculate similarity with input
-        inventory = InventoryCommunity.objects.last()
-        main_colors = inventory.get_main_colors_from_file(image)
-        vector = inventory.get_vector_from_colors(main_colors)
-        colors_frame = inventory.get_colors_frame()
+        main_colors = get_main_colors_from_file(image)
+        vector = get_vector_from_colors(main_colors)
+        # Get colors from community data and calculate similarities
+        # This loads all data into memory
+        content = list(community.kernel.content)
+        colors_frame = get_colors_frame(content)
         similarity = cosine_similarity(colors_frame, np.array(vector).reshape(1, -1)).flatten()
         # Find indices for ten most similar objects and sort by most similar
         indices = np.argsort(similarity)[-10:]
-        content = list(inventory.kernel.content)
         matches = [content[ix] for ix in indices]
         matches.reverse()
         # Create directory for input and copy matches there
         basename = os.path.basename(image)
         name, ext = os.path.splitext(basename)
-        dest = os.path.join(default_storage.location, "inventory", "colorz", name)
+        dest = os.path.join(default_storage.location, community.get_name(), "colorz", name)
         if not os.path.exists(dest):
             os.makedirs(dest)
         shutil.copy2(image, os.path.join(dest, basename))
