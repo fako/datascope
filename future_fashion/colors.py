fe3b77ad7bc288d6aebb596796ff62737684d227
Fako Berkers <email@fakoberkers.nl>
2018-06-23 13:19:38 +0200

    Handles empty colors and empty rgb values when creating a color frame

--- future_fashion/colors.py
+++ future_fashion/colors.py
@@ -13,11 +13,14 @@ def get_vector_from_colors(colors):
     return list(itertools.chain(*colors))
 
 
-def get_colors_frame(individuals):
-    records = [get_vector_from_colors(ind["colors"]) for ind in individuals if len(ind["colors"])]
+def get_colors_frame(content):
+    records = [
+        get_vector_from_colors(ind.get("colors", []))
+        for ind in content if len(ind.get("colors", []))
+    ]
     num_colors = int(len(records[0])/3)
     labels = []
     for ix in range(num_colors):
         ix = str(ix)
         labels += ["r"+ix, "g"+ix, "b"+ix]
-    return pd.DataFrame.from_records(records, columns=labels)
+    return pd.DataFrame.from_records(records, columns=labels).dropna(axis=0)
