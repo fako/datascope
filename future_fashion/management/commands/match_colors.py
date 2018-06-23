f48c7ccd37cb8b4e880e8b8d3257d993b2b5f8ce
Fako Berkers <email@fakoberkers.nl>
2018-06-23 13:20:04 +0200

    Save images differently depending on source

--- future_fashion/management/commands/match_colors.py
+++ future_fashion/management/commands/match_colors.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import logging
 
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
@@ -8,17 +9,43 @@ from django.core.files.storage import default_storage
 
 from core.management.commands import CommunityCommand
 from core.utils.configuration import DecodeConfigAction
-from future_fashion.models import InventoryCommunity
+from sources.models import ImageDownload
 from future_fashion.colors import get_main_colors_from_file, get_vector_from_colors, get_colors_frame
 
 
+log = logging.getLogger("datascope")
+
+
 class Command(CommunityCommand):
+    """
+    Example: ./manage.py match_colors FutureFashionCommunity -i ~/Downloads/fairy-tale.jpg -a tagged_kleding
+    """
 
     def add_arguments(self, parser):
         parser.add_argument('community', type=str, nargs="?", default=self.community_model)
         parser.add_argument('-a', '--args', type=str, nargs="*", default="")
         parser.add_argument('-c', '--config', type=str, action=DecodeConfigAction, nargs="?", default={})
         parser.add_argument('-i', '--image', type=str)
+
+    def handle_inventory_matches(self, matches, destination):
+        for ix, match in enumerate(matches):
+            name, ext = os.path.splitext(match["path"])
+            shutil.copy2(match["path"], os.path.join(destination, str(ix) + ext))
+
+    def handle_data_matches(self, matches, destination):
+        for ix, match in enumerate(matches):
+            uri = ImageDownload.uri_from_url(match["image"])
+            try:
+                download = ImageDownload.objects.get(uri=uri)
+            except ImageDownload.DoesNotExist:
+                continue
+            if not download.success:
+                continue
+            name, ext = os.path.splitext(download.body)
+            shutil.copy2(
+                os.path.join(default_storage.location, download.body),
+                os.path.join(destination, str(ix) + ext)
+            )
 
     def handle_community(self, community, *args, **options):
         # Get colors from input file
@@ -29,6 +56,7 @@ class Command(CommunityCommand):
         # This loads all data into memory
         content = list(community.kernel.content)
         colors_frame = get_colors_frame(content)
+        log.info("Color frame shape: {}".format(colors_frame.shape))
         similarity = cosine_similarity(colors_frame, np.array(vector).reshape(1, -1)).flatten()
         # Find indices for ten most similar objects and sort by most similar
         indices = np.argsort(similarity)[-10:]
@@ -39,8 +67,9 @@ class Command(CommunityCommand):
         name, ext = os.path.splitext(basename)
         dest = os.path.join(default_storage.location, community.get_name(), "colorz", name)
         if not os.path.exists(dest):
-            os.makedirs(dest)
+            os.makedirs(dest, exist_ok=True)
         shutil.copy2(image, os.path.join(dest, basename))
-        for ix, match in enumerate(matches):
-            name, ext = os.path.splitext(match["path"])
-            shutil.copy2(match["path"], os.path.join(dest, str(ix) + ext))
+        if community.get_name() == "fashion_data":
+            self.handle_data_matches(matches, dest)
+        else:
+            self.handle_inventory_matches(matches, dest)
