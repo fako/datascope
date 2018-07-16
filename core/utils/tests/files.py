1f6627f7c8ec142bddcbae4b6cc7d21294e02f5e
Fako Berkers <email@fakoberkers.nl>
2018-07-16 11:05:42 +0200

    Adds forgotten file for tests

---
+++ core/utils/tests/files.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+
+class TestFileSorter(TestCase):
+
+    def test_init(self):
+        self.skipTest("not tested")
+
+
+class TestFileBalancer(TestCase):
+
+    def test_init(self):
+        self.skipTest("not tested")
+
+
+class TestSemanticDirectoryScan(TestCase):
+
+    def test_init(self):
+        self.skipTest("not tested")
