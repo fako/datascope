826d4159c62ed0145ae66a9d42125a26ac2c399c
Fako Berkers <email@fakoberkers.nl>
2018-09-03 13:03:06 +0200

    Removes old view code used for clothing similarity search

--- future_fashion/views.py
+++ future_fashion/views.py
@@ -1,35 +1,11 @@
 import os
 import json
 
-from django.template.response import TemplateResponse
 from django.conf import settings
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
-from core.views.community import HtmlCommunityView
-
-
-class FutureFashionHtmlView(HtmlCommunityView):
-
-    def get(self, request, community_class, path=None, *args, **kwargs):
-        if not "$reference" in request.GET:
-            community = community_class.objects.last()
-            results = []
-            for ind in community.kernel.individual_set.all()[:10]:
-                ind.properties["id"] = ind.id
-                results.append(ind.properties)
-            return TemplateResponse(
-                request,
-                "{}/{}".format(community_class.get_name(), HtmlCommunityView.OK),
-                {
-                    'response': {
-                        'results': results
-                    }
-                }
-            )
-        return super(FutureFashionHtmlView, self).get(request, community_class, path=path, *args, **kwargs)
 
 
 @api_view(["POST", "OPTIONS"])
