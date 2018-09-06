a5108748f06783f557f214c5b241fc86969674b1
Fako Berkers <email@fakoberkers.nl>
2018-09-06 08:48:19 +0200

    Fixes endpoint for future fashion swipe interface

--- future_fashion/views.py
+++ future_fashion/views.py
@@ -32,30 +32,25 @@ class FutureFashionHtmlView(HtmlCommunityView):
         return super(FutureFashionHtmlView, self).get(request, community_class, path=path, *args, **kwargs)
 
 
-@api_view(["GET", "POST"])
+@api_view(["POST"])
 def swipe_interface_view(request):
 
     def format_data(entry):
         return {
             "id": entry["id"],
-            "url": request.build_absolute_uri(entry["path"].replace("system/files", ""))
+            "url": entry["path"].replace("system/files/", "")
         }
 
     with open(os.path.join(settings.PATH_TO_PROJECT, "future_fashion", "results", "little-french-dress.json")) as file:
         data = iter(json.load(file))
 
-    if request.method == "POST":
+    _id = request.data.get("id", None)
+    like = request.data.get("like", None)
+    score = request.data.get("score", None)
 
-        _id = request.GET.get("id", None)
-        like = request.GET.get("like", None)
-        score = request.GET.get("score", None)
-        if not _id or not like or not score or _id == "null" or like == "null" or score == "null":
-            return Response(
-                {"error": "The id, like and score parameters should be set when using POST"},
-                status.HTTP_400_BAD_REQUEST
-            )
+    if _id and like is not None and score is not None:
         if not isinstance(_id, int) and not _id.isnumeric() or not isinstance(like, int) and not like.isnumeric() or \
-                not isinstance(score, int) and not score.isnumeric():
+                not isinstance(score, float) and not score.isnumeric():
             return Response(
                 {"error": "The id, like and score parameters should be numbers"}, status.HTTP_400_BAD_REQUEST
             )
