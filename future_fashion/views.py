078d0ed189cc57d8e7897fcc15187d875b4c50a0
Fako Berkers <email@fakoberkers.nl>
2018-09-17 19:47:30 +0200

    Make the swipe endpoint into a carousel

--- future_fashion/views.py
+++ future_fashion/views.py
@@ -18,17 +18,34 @@ def swipe_interface_view(request):
     def format_data(entry):
         return {
             "id": entry["id"],
-            "url": entry["path"].replace("future_fashion/data", "")
+            "url": request.build_absolute_uri(entry["path"].replace("future_fashion/data", "")),
+            "type": entry["type"]
         }
 
+    # Standard request validation
+    _id = request.data.get("id", None)
+    like = request.data.get("like", None)
+    score = request.data.get("score", None)
+    type = request.GET.get("type", None)
+    if not type:
+        return Response(
+            {"error": "Type is a required parameter"}, status.HTTP_400_BAD_REQUEST
+        )
+    if _id and like is not None and score is not None:
+        # A post with set _id should have valid like and score as well
+        if not isinstance(_id, int) and not _id.isnumeric() or not isinstance(like, int) and not like.isnumeric() or \
+                not isinstance(score, float) and not score.isnumeric():
+            return Response(
+                {"error": "The id, like and score parameters should be numbers"}, status.HTTP_400_BAD_REQUEST
+            )
+
+    # API request validation
     api_view = CommunityView()
     configuration, created_at_info = api_view.get_configuration_from_request(request)
     configuration.update({
         "brighten": 50,
         "remove_background": 1
     })
-
-    # Checking validity of request
     for parameter in ["$top", "$bottom"]:
         # Check required configuration
         if parameter not in configuration:
@@ -36,7 +53,7 @@ def swipe_interface_view(request):
                 {"error": "The {} query parameter is required".format(parameter)}, status.HTTP_400_BAD_REQUEST
             )
         # Checking parameters and converting if necessary
-        parameter_value = configuration[parameter]
+        parameter_value = str(configuration[parameter])
         if parameter_value.startswith("#"):
             parameter_value = parameter_value[1:]
         if not hex_color_pattern.match(parameter_value):
@@ -58,25 +75,22 @@ def swipe_interface_view(request):
     )
     if not api_response.status_code == status.HTTP_200_OK:
         return api_response
-    data = iter(api_response.data["results"])
+    data = [entry for entry in api_response.data["results"] if entry["type"] == type]
+    if not _id:
+        return Response(format_data(data[0]), status.HTTP_200_OK)
 
-    # Now check exactly what data was asked for
-    _id = request.data.get("id", None)
-    like = request.data.get("like", None)
-    score = request.data.get("score", None)
+    # Figure out next object to return
+    # Should act like a carousel
+    # Initial position comes from _id
+    ixs = [entry["id"] for entry in data]
+    ix = ixs.index(_id)
+    if like:
+        ix += 1
+    else:
+        ix -= 1
+    if ix < 0:
+        ix = len(ixs) - 1
+    else:
+        ix %= len(ixs)
 
-    if _id and like is not None and score is not None:
-        if not isinstance(_id, int) and not _id.isnumeric() or not isinstance(like, int) and not like.isnumeric() or \
-                not isinstance(score, float) and not score.isnumeric():
-            return Response(
-                {"error": "The id, like and score parameters should be numbers"}, status.HTTP_400_BAD_REQUEST
-            )
-
-        while next(data)["id"] != int(_id):
-            pass
-
-    # Return the data unless it is exhausted
-    try:
-        return Response(format_data(next(data)), status.HTTP_200_OK)
-    except StopIteration:
-        return Response({}, status.HTTP_204_NO_CONTENT)
+    return Response(format_data(data[ix]), status.HTTP_200_OK)
