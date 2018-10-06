a58a756c6645fce23137da0e544706d23df0e200
Fako Berkers <email@fakoberkers.nl>
2018-10-06 22:37:24 +0200

    Tries to prevent deprecation warnings

--- core/utils/data/numeric_features.py
+++ core/utils/data/numeric_features.py
@@ -84,7 +84,7 @@ class NumericFeaturesFrame(object):
                 missing = series.index.difference(self.data[column].index)
                 new[series.name] = series.loc[list(missing.get_values())]
                 update[series.name] = series.loc[list(intersection.get_values())]
-        self.data = self.data.append(new)
+        self.data = self.data.append(new, sort=False)
         self.data.update(update)
 
     @staticmethod
