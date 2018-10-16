b4c966462ba65c1708de6c8286f911956555495b
Fako Berkers <email@fakoberkers.nl>
2018-10-16 13:44:51 +0200

    Initial work with making a discourse list view

--- online_discourse/urls.py
+++ online_discourse/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from core.views.community import CommunityView
 from online_discourse.models import DiscourseSearchCommunity
-from online_discourse.views import SearchDumpCommunityView, CreateDiscourseOrder
+from online_discourse.views import CreateDiscourseOrder, list_discourse_view
 
 
 urlpatterns = [
@@ -13,14 +13,13 @@ urlpatterns = [
         name=DiscourseSearchCommunity.get_name() + "_service"
     ),
     url(
-        r'^discourse-search/html/(?P<path>.+)?/?$',
-        SearchDumpCommunityView.as_view(),
-        kwargs={"community_class": DiscourseSearchCommunity},
-        name=DiscourseSearchCommunity.get_name() + "_html"
-    ),
-    url(
         r'^discourse-search/order/?$',
         CreateDiscourseOrder.as_view(),
         name=DiscourseSearchCommunity.get_name() + "_order"
+    ),
+    url(
+        r'^discourse-search/discourses/?$',
+        list_discourse_view,
+        name=DiscourseSearchCommunity.get_name() + "_list"
     ),
 ]
--- online_discourse/views.py
+++ online_discourse/views.py
@@ -1,21 +1,9 @@
 from django.core.mail import mail_managers
-from rest_framework import generics, throttling
+from rest_framework import generics, throttling, viewsets
+from rest_framework.response import Response
 
-from core.views.community import HtmlCommunityView
 from online_discourse.models.orders import DiscourseOrderSerializer
-
-
-class SearchDumpCommunityView(HtmlCommunityView):
-
-    @staticmethod
-    def data_for(community_class, response=None):
-        data = HtmlCommunityView.data_for(community_class, response)
-        if data is None:
-            data = {}
-        data["topics"] = {
-            "immigration"
-        }
-        return data
+from online_discourse.discourse import configurations, DiscourseConfiguration
 
 
 class AnonDiscourseOrderThrottle(throttling.AnonRateThrottle):
@@ -30,3 +18,19 @@ class CreateDiscourseOrder(generics.CreateAPIView):
         response = super().create(request, *args, **kwargs)
         mail_managers("discourse order!", "Please check the admin for order details")
         return response
+
+
+class DiscourseViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        discourses = {
+            name: discourse._asdict() for name, discourse in configurations.__dict__.items()
+            if isinstance(discourse, DiscourseConfiguration)
+        }
+        return Response(discourses)
+
+    def retrieve(self, request, pk=None):
+        pass
+
+
+list_discourse_view = DiscourseViewSet.as_view({"get": "list"})
