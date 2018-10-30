76e6f19bcc45280df923730186e3277d68fd73fa
Fako Berkers <email@fakoberkers.nl>
2018-10-30 16:48:58 +0100

    Solves problems with non-selection and the selection filter

--- core/processors/filter.py
+++ core/processors/filter.py
@@ -14,12 +14,18 @@ class FilterProcessor(Processor):
                 yield individual
 
     def select(self, individuals):
-        keys = self.config.select_keys
-        for individual in individuals:
-            for key in keys:
-                value = getattr(self.config, key, None)  # TODO: use config.get instead
-                if value is not None and key in individual and individual[key] == value:
-                    break
-            else:
-                continue
-            yield individual
+        criteria = {  # TODO: use config.get instead
+            key: getattr(self.config, key, None) for key in self.config.select_keys
+            if getattr(self.config, key, None)
+        }
+        if not criteria:
+            for individual in individuals:
+                yield individual
+        else:
+            for individual in individuals:
+                for key in criteria.keys():
+                    if key in individual and individual[key] == criteria[key]:
+                        break
+                else:
+                    continue
+                yield individual
