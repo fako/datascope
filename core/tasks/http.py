207b29f08ba9842f2f9b878340596dfca7954236
Fako Berkers <email@fakoberkers.nl>
2018-12-02 14:16:33 +0100

    Adds a possibility to set a wait interval when using send task just before a next_request

--- core/tasks/http.py
+++ core/tasks/http.py
@@ -86,6 +86,10 @@ def send(config, *args, **kwargs):
         # Prepare next request
         has_next_request = current_request = link.create_next_request()
         count += 1
+        # Take a break for scraping if configured
+        interval_duration = config.interval_duration / 1000
+        if has_next_request and interval_duration:
+            sleep(interval_duration)
     # Output results in simple type for json serialization
     return [success, errors]
 
