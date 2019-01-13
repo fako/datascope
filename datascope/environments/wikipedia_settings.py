8a548ff8f78cf58025aff2a07f5c9267170300b0
Fako Berkers <email@fakoberkers.nl>
2019-01-13 11:09:37 +0100

    Adds Celery beat to the project for Wikipedia cronjobs

--- datascope/environments/wikipedia_settings.py
+++ datascope/environments/wikipedia_settings.py
@@ -1,3 +1,5 @@
+from celery.schedules import crontab
+
 from datascope.settings_base import *
 
 DATABASES["default"]["ENGINE"] = 'django.db.backends.mysql'
@@ -11,6 +13,13 @@ STATIC_URL = "//tools-static.wmflabs.org/algo-news/static/"
 
 CELERY_DEFAULT_QUEUE = 'datascope'
 CELERY_BROKER_URL = 'redis://tools-redis:6379/0'
+CELERY_BEAT_SCHEDULE = {
+    "update_wiki_feed": {
+        "task": "wiki_feed.tasks.update_wiki_feed",
+        "schedule": crontab(minute=0, hour=6),
+    }
+}
+
 MAX_BATCH_SIZE = 100
 
 RAVEN_CONFIG = {
