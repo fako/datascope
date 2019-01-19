e9da7f7ee397220fc7c8e80097c7388fd5f20ad5
Fako Berkers <email@fakoberkers.nl>
2019-01-19 21:48:06 +0100

    Improves the DiscourseSearchCommunity pipeline

--- online_discourse/models/search.py
+++ online_discourse/models/search.py
@@ -4,6 +4,8 @@ import os
 from collections import OrderedDict
 from urlobject import URLObject
 from tqdm import tqdm
+from bs4 import BeautifulSoup
+from bs4.element import NavigableString
 
 try:
     import spacy
@@ -19,7 +21,6 @@ import json_field
 from core.models.organisms import Community, Collective, Individual
 from core.models.organisms.states import CommunityState
 from core.utils.helpers import cross_combine
-from online_discourse.models.sources import WebTextTikaResource
 from online_discourse.discourse import configurations
 from online_discourse.processors import TopicDetector, EntityDetector, OnlineDiscourseRankProcessor
 
@@ -50,7 +51,7 @@ class DiscourseSearchCommunity(Community):
             "schema": {},
             "errors": {},
         }),
-        ("download", {
+        ("download_article", {
             "process": "HttpResourceProcessor.fetch_mass",
             "input": "@search",
             "contribute": "Update:ExtractProcessor.pass_resource_through",
@@ -60,7 +61,26 @@ class DiscourseSearchCommunity(Community):
                 "_kwargs": {},
                 "_resource": "WebContentDownload",
                 "_update_key": "url",
-                "$language": "en"
+                "$language": "en",  # TODO: think of a way to include language as a parameter without using config
+                # Keys below are used by WebContentDownload to format its output
+                "resource_key": "resourcePath"
+            },
+            "schema": {},
+            "errors": {},
+        }),
+        ("download_home", {
+            "process": "HttpResourceProcessor.fetch_mass",
+            "input": "@search",
+            "contribute": "Update:ExtractProcessor.pass_resource_through",
+            "output": "@search",
+            "config": {
+                "_args": ["$.home"],
+                "_kwargs": {},
+                "_resource": "WebContentDownload",
+                "_update_key": "home",
+                # Keys below are used by WebContentDownload to format its output
+                "url_key": "home",
+                "resource_key": "home_resource"
             },
             "schema": {},
             "errors": {},
@@ -79,7 +99,8 @@ class DiscourseSearchCommunity(Community):
                     "#title": "$.title",
                     "#language": "$.Content-Language",
                     "#content": "$.X-TIKA:content",
-                    "#author": "$.author"
+                    "#author": "$.author",
+                    "#content_type": "$.Content-Type"
                 },
                 "_inline_key": "resourcePath"
             },
@@ -119,9 +140,9 @@ class DiscourseSearchCommunity(Community):
         "en": "english",  # grabs sklearn stopwords
         "nl": NL_STOP_WORDS
     }
-    NON_TOPICS = {
+    NON_TOPICS = {  # TODO: remove NON_TOPICS?
         "en": [],
-        "nl": ["cookies", "website", "browser", "advertenties"]
+        "nl": []
     }
 
     def initial_input(self, *args):
@@ -148,13 +169,81 @@ class DiscourseSearchCommunity(Community):
             )
         return collective
 
+    def _set_source_data(self, individual):
+        url = URLObject(individual.properties["url"])
+        source = url.hostname
+        if source.startswith("www."):
+            source = source.replace("www.", "", 1)
+        individual.properties["source"] = source
+        individual.properties["home"] = "{}://{}".format(url.scheme, url.hostname)
+        return individual
+
+    def _set_main_content(self, individual):
+
+        # Loading data
+        data = individual.properties.get("tika", {}) or {}
+        content_type = data.get("content_type", "text/html")
+        if not content_type.startswith("text/html"):
+            individual["content"] = data.get("content", "")
+            return individual
+        try:
+            with open(data["resourcePath"]) as fp:
+                article = BeautifulSoup(fp, "html5lib")
+            with open(individual.properties["home_resource"]) as fp:
+                home = BeautifulSoup(fp, "html5lib")
+        except FileNotFoundError:
+            log.warning("Missing a file for individual: {}".format(individual.id))
+            individual["content"] = ""
+            return individual
+
+        # Calculate text diff between home, article and Tika
+        raw_content = data.get("content")
+        home_contents = set()
+        for el in home.descendants:
+            if not isinstance(el, NavigableString):
+                continue
+            text = el.string.strip()
+            if text:
+                home_contents.add(text)
+        article_contents = []
+        junk_contents = []
+        for el in article.descendants:
+            if not isinstance(el, NavigableString):
+                continue
+            text = el.string.strip()
+            if text and text not in home_contents:
+                if text in raw_content:
+                    article_contents.append(text)
+                else:
+                    junk_contents.append(text)
+
+        individual.properties["junk"] = junk_contents
+        individual.properties["content"] = article_contents
+        return individual
+
+    def _set_author(self, individual):
+        data = individual.properties.get("content", {})
+        author = data.get("author", None)
+        if author and isinstance(author, str):
+            pass
+        elif author:
+            author = author[0]  # TODO: we take the primary author now, but should also consider other authors
+        else:
+            author = None
+        individual.properties["author"] = author
+        return individual
+
     def finish_search(self, out, err):
+        log.info("Filter duplicates and save source")
         total = out.individual_set.count()
         current_entry = None
         deletes = []
+        # Filter out duplicates, but keep track of terms where pages were found
+        # It also sets the source and home page URL for each result
         for entry in tqdm(out.individual_set.order_by("identity").iterator(), total=total):
             if current_entry is None or current_entry.identity != entry.identity:
                 if current_entry is not None:
+                    current_entry = self._set_source_data(current_entry)
                     current_entry.save()
                 if len(deletes):
                     out.individual_set.filter(id__in=deletes).delete()
@@ -166,7 +255,17 @@ class DiscourseSearchCommunity(Community):
                 current_entry.properties["term"].append(entry.properties["term"])
                 deletes.append(entry.id)
 
-    def finish_download(self, out, err):
+    def begin_download_home(self, out):
+        log.info("Resetting identifier to 'home'")
+        out.identifier = "home"
+        out.save()
+        total = out.individual_set.count()
+        for entry in tqdm(out.individual_set.iterator(), total=total):
+            entry.clean()
+            entry.save()
+
+    def begin_content(self, out):
+        log.info("Resetting identifier to 'resourcePath' and deleting non-content entries")
         out.identifier = "resourcePath"
         out.save()
         total = out.individual_set.count()
@@ -174,78 +273,64 @@ class DiscourseSearchCommunity(Community):
             entry.clean()
             entry.save()
         deletes = out.individual_set.filter(identity__isnull=True).delete()
-        log.info(deletes)
+        log.info("Deletes: {}".format(deletes))
 
+    def finish_content(self, out, err):
+        log.info("Deleting entries that are off topic entirely")
         # Now that text has been extracted. Delete irrelevant entries not about specified topics
         name, configuration = self.get_configuration_module()
         query_filter = Q()
         for topic in configuration.topics:
             query_filter |= Q(properties__icontains=topic)
         deletes = out.individual_set.exclude(query_filter).delete()
-        log.info(deletes)
+        log.info("Deletes: {}".format(deletes))
 
-    def finish_content(self, out, err):
-
-        spacy_parsers = {}
-        for language, package in self.SPACY_PACKAGES.items():
-            nlp = spacy.load(package)
-            nlp.add_pipe(ArguingLexiconParser(lang=nlp.lang))
-            spacy_parsers[language] = nlp
-
+        log.info("Resetting identifier to 'url' and cleaning output")
         # File paths to downloads is not a useful identifier after dataset creation
         # Resetting to source URL
         out.identifier = "url"
         out.save()
         total = out.individual_set.count()
-
         for individual in tqdm(out.individual_set.iterator(), total=total):
-
-            if individual.properties.get("source", None):
-                continue
-
-            source = URLObject(individual.properties["url"]).hostname
-            if source.startswith("www."):
-                source = source.replace("www.", "", 1)
-            individual.properties["source"] = source
-
             # Checking if there is any content data to work with.
             # Then undoing a weird hack where content data gets stored under resourcePath
             # It happens because inline_key and identifier need to match
             # TODO: allow inline_key to be different from identifier
             data = individual.properties.get("resourcePath", None)
-            if not isinstance(data, dict):
-                individual.clean()
-                individual.save()
-                continue
+            individual.properties["tika"] = data
+            if data is not None:
+                del individual.properties["resourcePath"]
+            individual.clean()
+            individual.save()
 
-            author = data.get("author", None)
-            if author and isinstance(author, str):
-                pass
-            elif author:
-                author = author[0]  # TODO: we take the primary author now, but should also consider other authors
-            else:
-                author = None
-            individual.properties["author"] = author
+        #################################################################################
+        # TODO: The next phases are better handled by a process_mini_batch task
+        #################################################################################
 
-            titles, paragraphs, junk = WebTextTikaResource.extract_texts(data.get("title"), data.get("content"))
-            del data["content"]
-            data["titles"] = titles
-            data["paragraphs"] = paragraphs
-            data["junk"] = junk
-            individual.properties["content"] = data
-            del individual.properties["resourcePath"]
+        log.info("Parsing content")
+        # Preparation by loading spaCy
+        spacy_parsers = {}
+        for language, package in self.SPACY_PACKAGES.items():
+            if language == self.config.language:  # TODO: allow multiple languages in arguing lexicon package
+                nlp = spacy.load(package)
+                nlp.add_pipe(ArguingLexiconParser(lang=nlp.lang))
+                spacy_parsers[language] = nlp
+        # Actual content extraction
+        for individual in tqdm(out.individual_set.iterator(), total=total):
+
+            individual = self._set_author(individual)
+            individual = self._set_main_content(individual)
 
             # Lots of code still depends on a property named "paragraph_groups".
             # This was used in an early version of text extraction
             # We mimick it here to keep backwards compatability
             # TODO: migrate code away from paragraph_groups
-            content = titles + paragraphs
+            content = individual.properties.get("content")
             if not len(content):
                 individual.clean()
                 individual.save()
                 continue
-            paragraph_groups = [content]
-            individual.properties["paragraph_groups"] = paragraph_groups
+            individual.properties["paragraph_groups"] = [content]
 
             nlp = spacy_parsers[self.config.language]
             argument_count = 0
@@ -260,17 +345,23 @@ class DiscourseSearchCommunity(Community):
             individual.clean()
             individual.save()
 
-        # And finally calculate the aggregates
+        #################################################################################
+        # Handling aggregations which is not supported by the framework at this time
+        #################################################################################
+
         self.set_aggregates(out)
         self.save()
 
     def set_aggregates(self, collection):
+        log.info("Aggregating sources and authors")
         self.aggregates.update(
             self.get_source_and_author_aggregates(collection)
         )
+        log.info("Aggregating topics")
         self.aggregates.update(
             self.get_topic_aggregates(collection)
         )
+        log.info("Aggregating entities")
         self.aggregates.update(
             self.get_entity_aggregates(collection)
         )
