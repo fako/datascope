e78f8c261f7b51a853cb5741c7464484d663db20
Fako Berkers <email@fakoberkers.nl>
2019-02-18 14:43:31 +0100

    Leaves urls to the project app and not to datagrowth related modules

--- datascope/urls.py
+++ datascope/urls.py
@@ -4,12 +4,36 @@ from django.contrib import admin
 from django.views import static
 admin.autodiscover()
 
-from core.urls import urlpatterns as core_patterns
-from future_fashion.urls import mediapatterns
+from datascope import views
+from core import views as core_views
+from wiki_feed.urls import urlpatterns as wiki_feed_patterns
+from visual_translations.urls import urlpatterns as visual_translations_patterns
+from future_fashion.urls import urlpatterns as future_fashion_patterns, mediapatterns
+from wiki_scope.urls import urlpatterns as topic_research_patterns
+from online_discourse.urls import urlpatterns as online_discourse_patterns
 
+
+legacy_patterns = [
+    url(r'^collective/(?P<pk>\d+)/content/$', core_views.CollectiveContentView.as_view(), name="collective-content"),
+    url(r'^collective/(?P<pk>\d+)/$', core_views.CollectiveView.as_view(), name="collective"),
+    url(r'^individual/(?P<pk>\d+)/content/$', core_views.IndividualContentView.as_view(), name="individual-content"),
+    url(r'^individual/(?P<pk>\d+)/$', core_views.IndividualView.as_view(), name="individual"),
+    url(r'^question/$', views.question, name="datascope-question")
+]
+
+legacy_patterns += wiki_feed_patterns
+legacy_patterns += visual_translations_patterns
+legacy_patterns += future_fashion_patterns
+legacy_patterns += topic_research_patterns
+legacy_patterns += online_discourse_patterns
+
+if settings.USE_MOCKS:
+    from core.tests.mocks.urls import urlpatterns as mock_patterns
+    legacy_patterns += mock_patterns
 
 urlpatterns = [
-    url(r'^data/v1/', include(core_patterns, namespace="v1")),
+    url(r'^$', views.index, name="datascope-index"),
+    url(r'^data/v1/', include(legacy_patterns, namespace="v1")),
     url(r'^admin/', include(admin.site.urls))
 ]
 
