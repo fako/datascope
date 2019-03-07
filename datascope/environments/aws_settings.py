2e5fd97222571ee4786eab6e3c1ac326cb203b13
Fako Berkers <email@fakoberkers.nl>
2019-03-07 15:03:20 +0100

    Switches AWS database for datascope to postgres

--- datascope/environments/aws_settings.py
+++ datascope/environments/aws_settings.py
@@ -1,6 +1,5 @@
 from datascope.settings_base import *
 
-DATABASES["default"]["ENGINE"] = 'django.db.backends.mysql'
 DATABASES["default"]["NAME"] = 'datascope'
 DATABASES["default"]["USER"] = 'django'
 DATABASES["default"]["PASSWORD"] = MYSQL_PASSWORD
