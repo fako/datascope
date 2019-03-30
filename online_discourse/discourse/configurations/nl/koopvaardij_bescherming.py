b1280b5486935f7dd3eff647d70bba2d1d0d2e10
Fako Berkers <email@fakoberkers.nl>
2019-03-30 15:01:50 +0100

    Adds koopvaardij bescherming debate

---
+++ online_discourse/discourse/configurations/nl/koopvaardij_bescherming.py
@@ -0,0 +1,81 @@
+from online_discourse.discourse import DiscourseConfiguration
+
+
+SINGULAR_SUBJECTS = [
+    "staat",
+    "verzekeraar",
+    "koopvaardij",
+    "piraat",
+    "militair",
+    "marine",
+    "schip",
+    "somalie",
+    "somalië",
+    "beveiliger",
+    "Koninklijke Vereniging van Nederlandse Reders",
+    "KVNR",
+    "Koninklijke Marine",
+    "Ministerie van Defensie",
+    "Verbond van Verzekeraars",
+    "PAX",
+    "TNI",
+    "AOU",
+    "Ministerie van Veiligheid en Justitie",
+    "private military companies"
+]
+
+PLURAL_SUBJECTS = [
+    "staten",
+    "landen",
+    "verzekeraars",
+    "piraten",
+    "militairen",
+    "mariniers",
+    "schepen",
+    "beveiligers"
+]
+
+DESCRIPTIVE_ADJECTIVES = [
+    "piraterij",
+    "private",
+    "geweld",
+    "bescherming",
+    "gewapend",
+    "geweldadig",
+    "veiligheid",
+    "handel",
+    "kosten",
+    "geld",
+    "losgeld",
+    "belastinginkomsten",
+    "geweldsmonopolie",
+    "zwaardmacht",
+    "symptoombestrijding",
+    "juridisch",
+    "wapen wedloop",
+    "anti-piraterij",
+    "scheepvaart",
+    "handelsbelangen",
+    "schade",
+    "machtsmisbruik",
+    "ontwikkelingshulp",
+    "vaarroute",
+    "onveilig",
+    "handelscorridor",
+    "internationaal recht",
+    "oorlog",
+    "winst",
+    "levensgevaarlijk",
+    "gevaarlijk"
+]
+
+koopvaardij_bescherming = DiscourseConfiguration(
+    title="Wet Bescherming Koopvaardij",
+    description="Deze wetswijziging maakt het mogelijk dat particulieren geweld mogen gebruiken "
+                "om schepen te beschermen tegen piraterij.",
+    language="nl",
+    topics=["piraat", "piraterij"],
+    singular_subjects=SINGULAR_SUBJECTS,
+    plural_subjects=PLURAL_SUBJECTS,
+    descriptive_adjectives=DESCRIPTIVE_ADJECTIVES
+)
