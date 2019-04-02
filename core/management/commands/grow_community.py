0ef94a53991855d5f7993e56aabc14e9ec5a81c3
Fako Berkers <email@fakoberkers.nl>
2019-04-02 15:31:30 +0200

    Fixes grow dataset and keep grow_community for legacy

---
+++ core/management/commands/grow_community.py
@@ -0,0 +1,19 @@
+import logging
+
+from core.management.commands import CommunityCommand
+
+
+log = logging.getLogger("datagrowth.command")
+
+
+class Command(CommunityCommand):
+    """
+    Grows a Community synchronously
+    """
+
+    def handle_community(self, community, *args, **options):
+        community.config = {"async": False}
+        community.save()
+        community.grow(*args)
+        log.info("Result: {}".format(community.kernel))
+        log.info("Growth: {}".format([growth.id for growth in community.growth_set.all()]))
--- datagrowth/management/commands/grow_dataset.py
+++ datagrowth/management/commands/grow_dataset.py
@@ -1,19 +1,22 @@
 import logging
 
-from core.management.commands import CommunityCommand
+from datagrowth.management.base import DatasetCommand
 
 
 log = logging.getLogger("datagrowth.command")
 
 
-class Command(CommunityCommand):
+class Command(DatasetCommand):
     """
-    Continuously polls a Community until it's completely grown.
+    Grows a Dataset synchronously
     """
 
-    def handle_community(self, community, *args, **options):
-        community.config = {"async": False}
-        community.save()
-        community.grow(*args)
-        log.info("Result: {}".format(community.kernel))
-        log.info("Growth: {}".format([growth.id for growth in community.growth_set.all()]))
+    def get_datasets(self):
+        raise TypeError("It is impossible to grow multiple datasets at the same time.")
+
+    def handle_community(self, dataset, *args, **options):
+        dataset.config = {"async": False}  # TODO: this is weird syntax as it is actually performing an update
+        dataset.save()
+        dataset.grow(*args)
+        log.info("Result: {}".format(dataset.kernel))
+        log.info("Growth: {}".format([growth.id for growth in dataset.growth_set.all()]))
