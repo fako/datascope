9766060be7d18c2429e04cc72259d1827070ee06
Fako Berkers <email@fakoberkers.nl>
2019-08-12 14:57:03 +0200

    Adds DataStorageAdmin class to the Datagrowth package

--- datagrowth/admin.py
+++ datagrowth/admin.py
@@ -1,2 +1,4 @@
 from datagrowth.resources.http.admin import HttpResourceAdmin
 from datagrowth.resources.shell.admin import ShellResourceAdmin
+
+from datagrowth.datatypes.documents.admin import DataStorageAdmin
