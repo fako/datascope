3b1aaa62028b9df43676d1f79cfc53019db362b1
Fako Berkers <email@fakoberkers.nl>
2019-08-19 13:01:43 +0200

    Introduces Document and Collection for the online discourse app

---
+++ online_discourse/models/storage.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey, ContentType
+
+from datagrowth.datatypes import CollectionBase, DocumentCollectionMixin, DocumentBase, DocumentPostgres
+
+
+class Collection(DocumentCollectionMixin, CollectionBase):
+
+    community = GenericForeignKey(ct_field="community_type", fk_field="community_id")
+    community_type = models.ForeignKey(ContentType, related_name="+")
+    community_id = models.PositiveIntegerField()
+
+    def init_document(self, data, collection=None):
+        Document = self.get_document_model()
+        return Document(
+            community=self.community,
+            collection=collection,
+            schema=self.schema,
+            properties=data
+        )
+
+
+class Document(DocumentPostgres, DocumentBase):
+
+    community = GenericForeignKey(ct_field="community_type", fk_field="community_id")
+    community_type = models.ForeignKey(ContentType, related_name="+")
+    community_id = models.PositiveIntegerField()
