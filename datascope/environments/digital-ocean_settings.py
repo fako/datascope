8a34d81fe286afa817bdbe20da85bd05590b2d76
Fako Berkers <email@fakoberkers.nl>
2019-08-19 13:09:01 +0200

    Updates the DS environment to connect to Postgres

--- datascope/environments/digital-ocean_settings.py
+++ datascope/environments/digital-ocean_settings.py
@@ -2,10 +2,6 @@ from celery.schedules import crontab
 
 from datascope.settings_base import *
 
-DATABASES["default"]["ENGINE"] = 'django.db.backends.mysql'
-DATABASES["default"]["NAME"] = 'datascope'
-DATABASES["default"]["USER"] = 'django'
-DATABASES["default"]["PASSWORD"] = MYSQL_PASSWORD
 
 STATIC_IP = "37.139.10.19"
 
