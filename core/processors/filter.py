d93cfe8db366266bbff505ab108be55c4919d328
Fako Berkers <email@fakoberkers.nl>
2019-08-25 12:21:42 +0200

    Renames internal filter processor methods to reflect what they do

--- core/processors/filter.py
+++ core/processors/filter.py
@@ -6,14 +6,14 @@ from datagrowth.datatypes import DocumentPostgres
 
 class FilterProcessor(QuerySetProcessor):
 
-    def get_query_filter_for_documents(self, criteria):
+    def get_query_filter_for_postgres(self, criteria):
         query_filter = Q()
         for key, values in criteria.items():
             for value in values:
                 query_filter |= Q(**{"properties__{}".format(key): value})
         return query_filter
 
-    def get_query_filter_for_individuals(self, criteria):
+    def get_query_filter_for_non_postgres(self, criteria):
         query_filter = Q()
         for key, values in criteria.items():
             for value in values:
@@ -25,6 +25,6 @@ class FilterProcessor(QuerySetProcessor):
             key: self.config.get(key).split("|") for key in self.config.select_keys
             if self.config.get(key, None)
         }
-        query_filter = self.get_query_filter_for_documents(criteria) if issubclass(query_set.model, DocumentPostgres) \
-            else self.get_query_filter_for_individuals(criteria)
+        query_filter = self.get_query_filter_for_postgres(criteria) if issubclass(query_set.model, DocumentPostgres) \
+            else self.get_query_filter_for_non_postgres(criteria)
         return query_set.filter(query_filter)
