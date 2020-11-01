01bbc2700ccf6e9fdaf35fe1f2dfa9ddd742ab0b
Fako Berkers <email@fakoberkers.nl>
2020-11-01 09:40:31 +0100

    Switches to host mode for now

--- src/datascope/settings_base.py
+++ src/datascope/settings_base.py
@@ -297,7 +297,7 @@ REST_FRAMEWORK = {
 }
 
 # Celery settings
-CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_RESULT_BACKEND = "django-db"
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
