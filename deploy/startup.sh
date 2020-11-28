a9d2ac0afc371b372d968cfe950ad881ac577b20
Fako Berkers <email@fakoberkers.nl>
2020-11-28 13:37:56 +0100

    Updates deploy tasks and adds a VPS startup script

---
+++ deploy/startup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+
+############################
+# Installation
+############################
+
+# Distribution software
+apt-get update
+apt-get install -y docker.io vim tmux python3-dev python3-pip python-is-python3 curl postgresql-client
+
+# Python packages
+pip3 install pip invoke docker-compose==1.26 ipython
+
+
+############################
+# Start services
+############################
+
+# Cronjobs
+update-rc.d cron defaults
+
+# Docker
+docker swarm init
+gcloud auth configure-docker -q
+
+
+############################
+# Start project
+############################
+
+cd /srv
+gsutil rsync -r -J gs://ds-deploy/ .
+cp server/datascope-cron /etc/cron.d/
+invoke pull
+invoke init production web
+invoke deploy
+
--- deploy/tasks.py
+++ deploy/tasks.py
@@ -2,8 +2,9 @@
 A file with tasks that can be executed on nodes
 This file needs to be compatible with invoke 0.11.0
 """
-from getpass import getpass
 from datetime import datetime
+from pprint import pprint
+from getpass import getpass
 try:
     from invoke import ctask as task
 except ImportError:
@@ -13,58 +14,148 @@ except ImportError:
 REPOSITORY = "eu.gcr.io/datascope-266618"
 
 
-@task()
-def init(ctx, mode, role):
-    assert role in ["worker", "web", f"Only 'web' and 'worker' are valid roles not {role}"]
-    ctx.run(f"cp environments/{mode}/secrets/.env .env")
-    ctx.run(f"cp docker-compose.{role}.yml docker-compose.yml")
+def get_versions_by_mode(ctx):
+    rsl = ctx.run("docker image ls", hide=True)
+    versions = {}
+    modes = {}
+    lines = rsl.stdout.split("\n")
+    for line in lines:
+        if not line.startswith(REPOSITORY):
+            continue
+        info = line.split()
+        if info[1] in ["prd", "acc"]:
+            modes[info[1]] = info[2]
+        else:
+            versions[info[2]] = info[1]
+    return {
+        mode: versions[image]
+        for mode, image in modes.items()
+    }
+
+
+def get_new_versions(ctx):
+    # Get the lastest local version of an image
+    local_images = ctx.run("docker image ls", hide=True)
+    last_local_image = local_images.stdout.split("\n")[1]
+    last_local_version_columns = last_local_image.split()
+    last_local_version = last_local_version_columns[1] if len(last_local_version_columns) else None
+    # List all versions of the images from the remote
+    versions = []
+    remote_images = ctx.run(f"gcloud container images list-tags {REPOSITORY}/datascope", hide=True)
+    lines = remote_images.stdout.split("\n")
+    for line in lines:
+        if line.startswith("DIGEST"):
+            continue
+        info = line.split()
+        if len(info) < 3:
+            continue
+        versions.append(info[1])
+    # Find the local version in the versions list and return everything that's newer
+    if last_local_version is not None:
+        last_local_version_ix = versions.index(last_local_version)
+        versions = versions[:last_local_version_ix+1]
+    return versions
 
 
 @task()
-def pull(ctx, version):
+def pull(ctx, version=None, latest_only=True):
+    if version is None:
+        version = get_new_versions(ctx)
+    else:
+        version = version.split(",")
+    if not len(version):
+        print("No new images to pull")
+        return
+
+    if latest_only:
+        version = version[:1]
+    print("Pulling:", version)
+    for tag in version:
+        ctx.run(
+            f"docker pull {REPOSITORY}/datascope:{tag}",
+            echo=True,
+            pty=True
+        )
+
+
+@task()
+def list(ctx):
+    ctx.run("docker image ls")
+    pprint(get_versions_by_mode(ctx))
+
+
+@task()
+def promote(ctx, version, mode="production"):
+
+    # Prepare the update
+    mode_to_tag = {
+        "production": "prd"
+    }
+    tag = mode_to_tag.get(mode, None)
+    assert tag is not None, f"Did not recognize mode {mode}"
+    # Run commands that update the image tags
     ctx.run(
-        f"docker pull {REPOSITORY}/datascope:{version}",
+        f"docker rmi {REPOSITORY}/datascope:{tag}",
+        echo=True,
+        warn=True,
+        hide="err"
+    )
+    ctx.run(
+        f"docker tag {REPOSITORY}/datascope:{version} {REPOSITORY}/datascope:{tag}",
         echo=True,
         pty=True
     )
 
 
 @task()
-def deploy(ctx, version, debug=False):
+def init(ctx, mode, role):
+    assert role in ["worker", "web", f"Only 'web' and 'worker' are valid roles not {role}"]
+    # Setup basic configuration
+    ctx.run(f"cp environments/{mode}/secrets/.env .env")
+    ctx.run(f"cp docker-compose.{role}.yml docker-compose.yml")
+    # Setup images
+    versions = get_new_versions(ctx)
+    if not len(versions):
+        print("No versions found in docker repository")
+        return
+    promote(ctx, versions[0])
+
+
+@task()
+def deploy(ctx):
+    versions = get_versions_by_mode(ctx)
     ctx.run(
-        ("export INVOKE_DJANGO_DEBUG=1 && " if debug else "") +
-        f"export RELEASE_VERSION={version} && "
-        "docker-compose -f docker-compose.yml down && "
-        "docker-compose -f docker-compose.yml up -d",
-        # "docker-compose -f docker-compose.yml config 2>/dev/null | "  # compiles configuration
-        # "docker stack deploy -c - --with-registry-auth --prune datascope",  # deploys latest "datascope" service
+        f"RELEASE_VERSION={versions['prd']} "
+        "docker-compose -f docker-compose.yml config 2>/dev/null | "  # compiles configuration
+        "docker stack deploy -c - --with-registry-auth --prune service ",
         echo=True,
     )
 
 
 @task()
-def migrate(ctx, version):
+def migrate(ctx, mode):
+    versions = get_versions_by_mode(ctx)
     postgres_password = getpass("Postgres password:")
     print("Running migration with root database user through docker-compose run ...")
     ctx.run(
-        f"export RELEASE_VERSION={version} && "
-        f"export INVOKE_POSTGRES_CREDENTIALS=postgres:{postgres_password} && "
-        "docker-compose -f docker-compose.yml run --rm datascope python manage.py migrate"
+        f"RELEASE_VERSION={versions['prd']} "
+        f"INVOKE_POSTGRES_CREDENTIALS=postgres:{postgres_password} "
+        f"docker-compose -f docker-compose.yml run --rm {mode} python manage.py migrate"
     )
 
 
 @task()
-def run(ctx, version):
-    print("Please run the following directly to access a container directly")
-    print(
-        f"export RELEASE_VERSION={version} && "
-        "docker-compose -f docker-compose.yml run --rm datascope bash"
+def run(ctx, mode):
+    versions = get_versions_by_mode(ctx)
+    print(f"Starting a bash shell in a {mode} container through docker-compose run ...")
+    ctx.run(
+        f"RELEASE_VERSION={versions['prd']} "
+        f"docker-compose -f docker-compose.yml run --rm {mode} bash"
     )
 
 
 @task()
 def publish_scripts(ctx):
-    ctx.run("rm -f deploy/.env")
     ctx.run("gsutil rsync -rd -J deploy gs://ds-deploy/")
 
 
@@ -75,7 +166,15 @@ def update_scripts(ctx):
 
 @task()
 def prune(ctx):
-    ctx.run("docker system prune --all")
+    versions = get_versions_by_mode(ctx)
+    ctx.run("docker system prune --all")  # this clears everything also tags!
+    # Now we bring back the necessary tags
+    for mode, version in versions.items():
+        ctx.run(
+            f"docker tag {REPOSITORY}/datascope:{mode} {REPOSITORY}/datascope:{version}",
+            echo=True,
+            pty=True
+        )
 
 
 @task()
