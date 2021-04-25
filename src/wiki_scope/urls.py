22a58fce960501bc7357cc671c6e50a8e4eb52af
Fako Berkers <email@fakoberkers.nl>
2021-04-25 09:37:15 +0200

    Deletes unused url patterns and views

--- src/wiki_scope/urls.py
+++ src/wiki_scope/urls.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals, absolute_import, print_function, division
-
 from django.conf.urls import url
 
 from core.views.community import CommunityView, HtmlCommunityView
