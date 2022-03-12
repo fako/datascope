4cee725b4ba30a0c64e1053628a23b046fe8d53e
Fako Berkers <email@fakoberkers.nl>
2022-03-12 19:00:11 +0100

    Adds bash script to generate visual_translations

---
+++ src/visual_translations/generate.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ..
+python manage.py generate_eu_visual_translations -a immigrants -d
+python manage.py generate_eu_visual_translations -a privatization
+python manage.py generate_eu_visual_translations -a peace
+python manage.py generate_eu_visual_translations -a cowshed
+python manage.py generate_eu_visual_translations -a pension
+python manage.py generate_eu_visual_translations -a women
+python manage.py generate_eu_visual_translations -a nature
+python manage.py generate_eu_visual_translations -a credit
+python manage.py generate_eu_visual_translations -a leave
+python manage.py generate_eu_visual_translations -a law
+python manage.py generate_eu_visual_translations -a justice
+python manage.py generate_eu_visual_translations -a virus
+python manage.py generate_eu_visual_translations -a vaccine
+python manage.py generate_eu_visual_translations -a corona
+python manage.py generate_eu_visual_translations -a brexit
+cd -
