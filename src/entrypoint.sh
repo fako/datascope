83a1a9dac98be4e29ab74e5ce73ffb02bca9af97
Fako Berkers <email@fakoberkers.nl>
2024-04-24 14:44:37 +0200

    Fixes datagrowth package detection in entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,7 +5,7 @@
 set -e
 
 
-if [ "$INVOKE_DJANGO_DEBUG" == "1" ]  && [ -d "/usr/src/datagrowth" ] && \
+if [ "$INVOKE_DJANGO_DEBUG" == "1" ]  && [ -e "/usr/src/datagrowth/setup.py" ] && \
     [ $(pip show datagrowth | grep "Location:" | awk -F "/" '{print $NF}') == "site-packages" ]
 then
     echo "Replacing datagrowth PyPi installation with editable version"
