2fd5b47070e025f9680dddfb88ace708677adb41
Fako Berkers <email@fakoberkers.nl>
2024-04-26 20:39:18 +0200

    Sets up new Google tracker for home landing page

--- src/apps/static/apps/datascope/scripts/main.js
+++ src/apps/static/apps/datascope/scripts/main.js
@@ -1,10 +1,19 @@
 $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+    /*****************************
+     * Navigation
+     *****************************/
+
+    $('a[href*=\\#]:not([href=\\#])').click(function() {
+
+        if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') &&
+            location.hostname === this.hostname)
+        {
+
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
             if (target.length) {
-                _paq.push(['trackPageView', this.hash]);
+                gtag('event', 'page_view', { 'page_path': this.hash });
                 $('#navbar-collapse-main').collapse('hide');
                 $('html,body').animate({
                     scrollTop: target.offset().top - parseInt(target.css('padding-top'))
@@ -13,6 +22,14 @@ $(function() {
             }
         }
 
+    });
+
+    $('.social a').click(function(event) {
+        gtag('event', 'social_click', {  // "social_click" is a custom event name
+            'event_category': 'Contact',  // Categorize this event
+            'event_label': event.currentTarget.getAttribute('href'),  // Use the href as the label
+            'transport_type': 'beacon'  // Ensures the event is sent before the page unloads
+        });
     });
 
 });
